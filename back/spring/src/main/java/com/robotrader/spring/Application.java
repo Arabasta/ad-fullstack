e3e268a5ea9d3dcd3a2102de3a8ca58d576e60e3
Jefri <jefritoo@gmail.com>
2024-07-22 22:13:09 +0800

    back spring: rename directory

--- back/spring/src/main/java/com/robotrader/spring/Application.java
+++ back/spring/src/main/java/com/robotrader/spring/Application.java
@@ -1,4 +1,4 @@
-package com.ecommerce.spring;
+package com.robotrader.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
