19a461cf245f3ef3847cb3a700fc6a71e4e22b29
zephyrdark <zephyrdark@users.noreply.github.com>
2024-07-23 19:19:53 +0800

    back-fastApi-price_predictor: clean up, removed model.py, reduced requirements.txt

--- back/fastApi/price_predictor/README.md
+++ back/fastApi/price_predictor/README.md
@@ -12,9 +12,8 @@ _ref: https://fastapi-openapi-markdown-documentation-generator-d96hc549nb58e.str
 Send GET or POST requests to the api at `http://{HOST_IP_ADDRESS}:8000/api/{API_METHOD}` (or ip  host).
 
 1. GET http://localhost:8000/documentation
-2. GET http://localhost:8000/api/train
-3. GET http://localhost:8000/api/predictions
-4. POST http://localhost:8000/api/predictions/{ticker}
+2. GET http://localhost:8000/api/predictions
+3. POST http://localhost:8000/api/predictions/{ticker}
 
 ---
 ## API
--- back/fastApi/price_predictor/main.py
+++ back/fastApi/price_predictor/main.py
@@ -2,21 +2,15 @@
 from fastapi import FastAPI, Path
 from fastapi.responses import RedirectResponse
 from typing import Annotated
-# import tensorflow as tf
 import json
 import src.utils as utils
-import model_lstm  # this is to import the .py model file. Change filepath as needed.
 
 app = FastAPI(docs_url="/documentation", redoc_url=None)
 
 # Constants
-# TODO: consider programmatically update file name
-PREDICTIONS_JSON_TEXT = "predictions_json.txt"
+CONSOLIDATED_PREDICTIONS = "consolidated.txt"
 PARENT_DIRECTORY_PATH = str(utils.get_project_root())
 REPO_ROOT_PATH = str(utils.get_repo_root())
-# TODO: replace with pulling model from S3 bucket.
-# TODO: programmatically set .keras model name.
-# MODEL = tf.keras.models.load_model(f'{REPO_ROOT_PATH}/ml/price_predictor/models/SPY-1721708792-model-rmse_14.keras')
 
 
 @app.get("/")
@@ -24,23 +18,15 @@ async def redirect_to_documentation():
     return RedirectResponse("http://localhost:8000/documentation")
 
 
-@app.get("/train/")
-async def train():
-    await model_lstm.train()
-    return {"Message": "Training Started"}
-
-
-@app.get("/predictions/")
+@app.get("/api/predictions/")
 async def predictions():
-    # f = open(f'{PREDICTIONS_JSON_TEXT}', "r")
-    with open(f'{PARENT_DIRECTORY_PATH}/predictions/{PREDICTIONS_JSON_TEXT}') as f:
+    with open(f'{PARENT_DIRECTORY_PATH}/predictions/{CONSOLIDATED_PREDICTIONS}') as f:
         json_data = json.load(f)
-    return {"predictions": json_data}  # test implementation with postman
+    return {"predictions": json_data}
 
 
-@app.post("/predictions/{ticker}")
+@app.post("/api/predictions/{ticker}")
 async def predictions(ticker: Annotated[str, Path(title="Input TICKER (e.g., SPY) to get predicted prices")],):
     with open(f'{PARENT_DIRECTORY_PATH}/predictions/{ticker.upper()}.txt') as f:
         json_data = json.load(f)
-    return {"predictions": json_data}  # test implementation with postman
-# to run server: `fastapi run`
+    return {"predictions": json_data}
