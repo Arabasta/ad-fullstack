7aa52abafa3c534c3864a4c40ed11eee01f20a7a
Arabasta <keiyamx@gmail.com>
2024-07-23 15:04:56 +0800

    back spring: removed ecommerce dir

--- back/spring/src/main/java/com/robotrader/spring/controller/HealthController.java
+++ back/spring/src/main/java/com/robotrader/spring/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.ecommerce.spring.controller.health;
+package com.robotrader.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
