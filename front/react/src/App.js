35f883ddf4184a1928ae5ae9deb425f819ef5545
luca <xiongkun778@163.com>
2024-07-25 02:39:25 +0800

    front react:finish navi bar for web(not styling)

--- front/react/src/App.js
+++ front/react/src/App.js
@@ -4,11 +4,13 @@ import { AuthProvider } from './context/AuthContext';
 import ErrorBoundary from './components/common/error/ErrorBoundary';
 import authRoutes from './routes/AuthRoutes';
 import mainRoutes from './routes/MainRoutes';
+import NavigationBarForWeb from './components/layout/NavigationBarForWeb';
 
 const App = () => {
     return (
         <AuthProvider>
             <Router>
+                <NavigationBarForWeb />
                 <ErrorBoundary>
                     <Routes>
                         {authRoutes}
--- front/react/src/routes/MainRoutes.js
+++ front/react/src/routes/MainRoutes.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import NewsPage from '../pages/NewsPage';
 import NotFoundPage from '../pages/error/NotFoundPage';
 import LandingPage from "../pages/LandingPage";
+import WalletPage from "../pages/WalletPage";
+import SupportPage from "../pages/SupportPage";
+import SettingsPage from "../pages/SettingsPage";
+
 
 const mainRoutes = [
     <Route key="landing" path="/" element={<LandingPage />} />,
     <Route key="home" path="/home" element={<HomePage />} />,
+    <Route key="login" path="/news" element={<NewsPage />} />,
+    <Route key="wallet" path="/wallet" element={<WalletPage />} />,
+    <Route key="support" path="/support" element={<SupportPage />} />,
+    <Route key="settings" path="/settings" element={<SettingsPage />} />,
     <Route key="not-found" path="*" element={<NotFoundPage />} />
 ];
 
