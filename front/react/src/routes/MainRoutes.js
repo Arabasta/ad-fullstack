1ff20d9004f0e9b5576783a6ebc47bf66735b74e
luca <xiongkun778@163.com>
2024-07-25 02:39:25 +0800

    front react:rebase with master

--- front/react/src/routes/MainRoutes.js
+++ front/react/src/routes/MainRoutes.js
@@ -6,6 +6,11 @@ import LandingPage from "../pages/navbar/LandingPage";
 
 const mainRoutes = [
     <Route key="landing" path="/" element={<LandingPage />} />,
+    <Route key="home" path="/home" element={<HomePage />} />,
+    <Route key="login" path="/news" element={<NewsPage />} />,
+    <Route key="wallet" path="/wallet" element={<WalletPage />} />,
+    <Route key="support" path="/support" element={<SupportPage />} />,
+    <Route key="settings" path="/settings" element={<SettingsPage />} />,
     <Route key="dashboard" path="/dashboard" element={<DashboardPage />} />,
     <Route key="not-found" path="*" element={<NotFoundPage />} />
 ];
