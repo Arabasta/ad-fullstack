dffb2bd4c6c260d23d6afaef35e81db76c53c7a0
luca <xiongkun778@163.com>
2024-07-25 01:55:31 +0800

    front react:add some pages

---
+++ front/react/src/pages/SupportPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SupportPage = () => {
+    return (
+        <div>
+            <h2>Support</h2>
+            <p>Welcome to the Support page! For Test Only</p>
+        </div>
+    );
+};
+
+export default SupportPage;
