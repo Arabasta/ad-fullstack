04855c723d71e0e8ca4022c1faff0788247734e3
luca <xiongkun778@163.com>
2024-07-25 01:55:31 +0800

    front react:add some pages

---
+++ front/react/src/pages/SettingsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SettingsPage = () => {
+    return (
+        <div>
+            <h2>Home</h2>
+            <p>Welcome to the settings page! For Test Only</p>
+        </div>
+    );
+};
+
+export default SettingsPage;
