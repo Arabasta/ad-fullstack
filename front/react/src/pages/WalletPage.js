07a5fe311d49167653a54bdcb2b5bcaccb1f28d4
luca <xiongkun778@163.com>
2024-07-25 01:55:31 +0800

    front react:add some pages

---
+++ front/react/src/pages/WalletPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WalletPage = () => {
+    return (
+        <div>
+            <h2>Wallet</h2>
+            <p>Welcome to the Wallet page! For Test Only</p>
+        </div>
+    );
+};
+
+export default WalletPage;
