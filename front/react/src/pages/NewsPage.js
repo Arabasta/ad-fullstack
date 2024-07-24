63589879d830f5d8b97de3bd68d6723f7114889a
luca <xiongkun778@163.com>
2024-07-25 01:55:31 +0800

    front react:add some pages

---
+++ front/react/src/pages/NewsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewsPage = () => {
+    return (
+        <div>
+            <h2>News page</h2>
+            <p>Welcome to the News page! For Test Only</p>
+        </div>
+    );
+};
+
+export default NewsPage;
