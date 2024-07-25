76d478ce8c00fd217b4075baf8b5ba9b32e71742
Arabasta <keiyamx@gmail.com>
2024-07-26 00:08:18 +0800

    back spring: registration use case extended
    - added createCustomerUser to UserService
    - added customer fields to RegistrationRequest
    - moved PasswordEncoder to separate file to avoid cyclical dependency issue

--- back/spring/src/main/java/com/robotrader/spring/dto/auth/RegistrationRequest.java
+++ back/spring/src/main/java/com/robotrader/spring/dto/auth/RegistrationRequest.java
@@ -20,4 +20,21 @@ public class RegistrationRequest {
     @NotBlank
     @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 7, max = 15, message = "Please input a valid mobile number")
+    private String mobileNumber;
+
+    @NotBlank
+    @Size(min = 1, max = 50, message = "First Name cannot be more than 50 characters")
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 1, max = 50, message = "Last Name cannot be more than 50 characters")
+    private String lastName;
+
+    @NotBlank
+    @Size(min = 1, max = 50, message = "Nationality cannot be more than 50 characters")
+    private String nationality;
+
 }
--- back/spring/src/main/java/com/robotrader/spring/service/UserService.java
+++ back/spring/src/main/java/com/robotrader/spring/service/UserService.java
@@ -1,22 +1,32 @@
 package com.robotrader.spring.service;
 
+import com.robotrader.spring.dto.auth.RegistrationRequest;
+import com.robotrader.spring.dto.auth.RegistrationResponse;
 import com.robotrader.spring.interfaces.IUserService;
+import com.robotrader.spring.model.Customer;
 import com.robotrader.spring.model.User;
+import com.robotrader.spring.model.enums.RoleEnum;
 import com.robotrader.spring.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements IUserService, UserDetailsService {
 
     private final UserRepository userRepository;
+    private final CustomerService customerService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, CustomerService customerService, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.customerService = customerService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -36,5 +46,20 @@ public class UserService implements IUserService, UserDetailsService {
             throw new UsernameNotFoundException("User not found");
 
         return user;
+    }
+
+    @Transactional
+    public RegistrationResponse createCustomerUser(RegistrationRequest registrationRequest) {
+        User user = new User();
+        user.setUsername(registrationRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+        user.setEmail(registrationRequest.getEmail());
+        user.setRole(RoleEnum.ROLE_CUSTOMER);
+
+        Customer customer = customerService.createCustomerUser(registrationRequest);
+        user.setCustomer(customer);
+
+        save(user);
+        return new RegistrationResponse("User registered successfully", user.getId());
     }
 }
