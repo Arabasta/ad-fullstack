9879b2097aabd904203d8e1362dacfe21eecb683
luca <xiongkun778@163.com>
2024-07-28 11:44:10 +0800

    front react: revise some buttons2

--- front/react/src/routes/NaviRoutes.js
+++ front/react/src/routes/NaviRoutes.js
@@ -3,12 +3,13 @@ import {Route} from "react-router-dom";
 import NewsPage from "../pages/navbar/NewsPage";
 import SupportPage from "../pages/navbar/SupportPage";
 import WalletPage from "../pages/navbar/WalletPage";
+import SettingsPage from "../pages/navbar/SettingsPage";
 
 const mainRoutes = [
     <Route key="news" path="/news" element={<NewsPage />} />,
     <Route key="support" path="/support" element={<SupportPage />} />,
     <Route key="wallet" path="/wallet" element={<WalletPage />} />,
-    <Route key="settings" path="/settings" element={<settingsPage />} />
+    <Route key="settings" path="/settings" element={<SettingsPage />} />
 
 ];
 
