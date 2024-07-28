d3a2747a257dcd249c944e86438e77eb318f6687
Jefri <jefritoo@gmail.com>
2024-07-29 00:04:06 +0800

    back spring trading: refactor backtesting and tradeapp

--- back/spring/src/main/java/com/robotrader/spring/trading/BackTesting.java
+++ back/spring/src/main/java/com/robotrader/spring/trading/BackTesting.java
@@ -1,19 +1,53 @@
 package com.robotrader.spring.trading;
 
+import com.robotrader.spring.trading.algorithm.TradingAlgorithmOne;
+import com.robotrader.spring.trading.dto.LiveMarketData;
 import com.robotrader.spring.trading.dto.TradeTransaction;
 import com.robotrader.spring.trading.algorithm.TradingAlgorithm;
+import com.robotrader.spring.trading.service.MarketDataService;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+@Service
 public class BackTesting {
+    private final MarketDataService marketDataService;
+    private TradingAlgorithm tradingAlgorithm;
+    private List<BigDecimal> pricePredictions;
+    @Setter
+    private Map<String, List<Object>> marketDataHistory;
 
-    public BackTesting() {}
+    @Autowired
+    public BackTesting(MarketDataService marketDataService) {
+        this.marketDataService = marketDataService;
+    }
 
-    public void run(TradingAlgorithm tradingAlgorithm, List<BigDecimal> pricePredictions, Map<String, List<Object>> marketDataHistory) {
+    public void run(TradingAlgorithm tradingAlgorithm) {
+        this.tradingAlgorithm = tradingAlgorithm;
+
+        marketDataService.getHistoricalMarketData(tradingAlgorithm.getTicker())
+                .doOnNext(this::setMarketDataHistory)
+                .doOnNext(data -> getPricePredictions())
+                .doOnNext(data -> runSimulation())
+                .doOnNext(data -> saveTransaction())
+                .subscribe(
+                        data -> System.out.println("Backtesting completed successfully."),
+                        error -> {
+                            System.err.println("Error during backtesting: " + error.getMessage());
+                            error.printStackTrace();
+                        }
+                );
+    }
+
+     public void runSimulation() {
         // Loop through price history and execute algo, simulating progress of time
         while (!pricePredictions.isEmpty()) {
             tradingAlgorithm.setPricePredictions(new ArrayList<>(pricePredictions));
@@ -26,6 +60,16 @@ public class BackTesting {
             marketDataHistory.get("high").remove(0);
             marketDataHistory.get("low").remove(0);
         }
+    }
+
+    public void getPricePredictions() {
+        List<Object> objects = marketDataHistory.get("close");
+        pricePredictions = objects.stream()
+                .map(price -> (BigDecimal) price)
+                .collect(Collectors.toList()); //TODO: Predictions == history for now
+    }
+
+    public void saveTransaction() {
         List<TradeTransaction> trades = tradingAlgorithm.getTradeTransactions();
         TradeTransaction lastTrade = null;
         BigDecimal totalProfit = BigDecimal.ZERO;
