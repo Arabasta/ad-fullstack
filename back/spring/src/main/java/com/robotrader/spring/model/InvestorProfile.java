7101b5dc8544cbd2627c00fefde1c8920dd4e62d
Arabasta <keiyamx@gmail.com>
2024-07-30 01:52:04 +0800

    back spring: overhauled InvestorProfile entity

--- back/spring/src/main/java/com/robotrader/spring/model/InvestorProfile.java
+++ back/spring/src/main/java/com/robotrader/spring/model/InvestorProfile.java
@@ -1,13 +1,19 @@
 package com.robotrader.spring.model;
 
-import jakarta.persistence.*;
+import com.robotrader.spring.model.enums.PortfolioTypeEnum;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -16,18 +22,86 @@ public class InvestorProfile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Integer withdrawalScore;
+    // Question 1
+    @Min(value = 1, message = "Please input a value between 1 and 4")
+    @Max(value = 4, message = "Please input a value between 1 and 4")
+    @NotNull
+    private Integer investmentDurationScore = 0;
 
-    private Integer expenditureScore;
+    // Question 2
+    @Min(value = 1, message = "Please input a value between 1 and 4")
+    @Max(value = 4, message = "Please input a value between 1 and 4")
+    @NotNull
+    private Integer withdrawalSpendingPlanScore = 0;
 
-    private Integer knowledgeScore;
+    // Question 3
+    @Min(value = 1, message = "Please input a value between 1 and 4")
+    @Max(value = 4, message = "Please input a value between 1 and 4")
+    @NotNull
+    private Integer investmentKnowledgeScore = 0;
 
-    private Integer riskScore;
+    // Question 4
+    @Min(value = 1, message = "Please input a value between 1 and 3")
+    @Max(value = 3, message = "Please input a value between 1 and 3")
+    @NotNull
+    private Integer riskRewardScore = 0;
 
-    private Integer experienceScore;
+    // Question 5
+    @Min(value = 1, message = "Please input a value between 1 and 3")
+    @Max(value = 3, message = "Please input a value between 1 and 3")
+    @NotNull
+    private Integer ownedInvestmentsScore = 0;
 
-    private Integer scenarioScore;
+    // Question 6
+    @Min(value = 1, message = "Please input a value between 1 and 4")
+    @Max(value = 4, message = "Please input a value between 1 and 4")
+    @NotNull
+    private Integer investmentPersonalityScore = 0;
 
-    private Integer chartScore;
+    // derived field
+    private Integer calculatedTimeHorizonScore = 0;
+
+    // derived field
+    private Integer calculatedRiskToleranceScore = 0;
+
+    // derived field
+    @Setter
+    private PortfolioTypeEnum recommendedPortfolioType;
+
+    public void setInvestmentDurationScore(Integer investmentDurationScore) {
+        this.calculatedTimeHorizonScore -= this.investmentDurationScore;
+        this.calculatedTimeHorizonScore += investmentDurationScore;
+        this.investmentDurationScore = investmentDurationScore;
+    }
+
+    public void setWithdrawalSpendingPlanScore(Integer expenditureScore) {
+        this.calculatedTimeHorizonScore -= this.withdrawalSpendingPlanScore;
+        this.calculatedTimeHorizonScore += expenditureScore;
+        this.withdrawalSpendingPlanScore = expenditureScore;
+    }
+
+    public void setInvestmentKnowledgeScore(Integer knowledgeScore) {
+        this.calculatedRiskToleranceScore -= this.investmentKnowledgeScore;
+        this.calculatedRiskToleranceScore += knowledgeScore;
+        this.investmentKnowledgeScore = knowledgeScore;
+    }
+
+    public void setRiskRewardScore(Integer riskScore) {
+        this.calculatedRiskToleranceScore -= this.riskRewardScore;
+        this.calculatedRiskToleranceScore += riskScore;
+        this.riskRewardScore = riskScore;
+    }
+
+    public void setOwnedInvestmentsScore(Integer experienceScore) {
+        this.calculatedRiskToleranceScore -= this.ownedInvestmentsScore;
+        this.calculatedRiskToleranceScore += experienceScore;
+        this.ownedInvestmentsScore = experienceScore;
+    }
+
+    public void setInvestmentPersonalityScore(Integer scenarioScore) {
+        this.calculatedRiskToleranceScore -= this.investmentPersonalityScore;
+        this.calculatedRiskToleranceScore += scenarioScore;
+        this.investmentPersonalityScore = scenarioScore;
+    }
 
 }
