b7e1aa0c4a715050cab72aa29761ee41edfb83f6
Arabasta <keiyamx@gmail.com>
2024-07-29 21:30:04 +0800

    front react: simplified AuthContext, removed user state

--- front/react/src/context/AuthContext.js
+++ front/react/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 import authenticationService from '../services/auth/AuthenticationService';
 
 // used to store auth state and methods across component tree
@@ -6,35 +6,20 @@ import authenticationService from '../services/auth/AuthenticationService';
 export const AuthContext = createContext(undefined);
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        // check for stored user in local storage
-        const user = localStorage.getItem('user');
-        if (user) {
-            setUser(JSON.parse(user));
-        }
-    }, []);
-
     const register = async (registrationData) => {
-        const response = await authenticationService.register(registrationData);
-        setUser(response);
-        localStorage.setItem('user', JSON.stringify(response));
+        return authenticationService.register(registrationData);
     };
 
     const login = async (username, password) => {
-        const response = await authenticationService.login(username, password);
-        setUser(response);
-        localStorage.setItem('user', JSON.stringify(response));
+        return authenticationService.login(username, password);
     };
 
     const logout = () => {
         authenticationService.logout();
-        setUser(null);
     };
 
     return (
-        <AuthContext.Provider value={{ user, setUser, register, login, logout }}>
+        <AuthContext.Provider value={{ register, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
