2d08cd9196d9aa0826253671d201d999e3f3897a
Arabasta <keiyamx@gmail.com>
2024-07-30 01:53:51 +0800

    back spring: added InvestorProfile GET and Update, service methods and controller endpoints

--- back/spring/src/main/java/com/robotrader/spring/service/InvestorProfileService.java
+++ back/spring/src/main/java/com/robotrader/spring/service/InvestorProfileService.java
@@ -1,7 +1,10 @@
 package com.robotrader.spring.service;
 
+import com.robotrader.spring.dto.investorProfile.InvestorProfileDTO;
+import com.robotrader.spring.dto.investorProfile.InvestorProfileUpdateDTO;
 import com.robotrader.spring.exception.notFound.InvestorProfileNotFoundException;
 import com.robotrader.spring.model.InvestorProfile;
+import com.robotrader.spring.model.enums.PortfolioTypeEnum;
 import com.robotrader.spring.repository.InvestorProfileRepository;
 import com.robotrader.spring.service.interfaces.ICustomerService;
 import com.robotrader.spring.service.interfaces.IInvestorProfileService;
@@ -15,7 +18,7 @@ public class InvestorProfileService implements IInvestorProfileService {
     private final InvestorProfileRepository investorProfileRepository;
     private final ICustomerService customerService;
 
-    public InvestorProfileService(InvestorProfileRepository investorProfileRepository, @Lazy CustomerService customerService) {
+    public InvestorProfileService(InvestorProfileRepository investorProfileRepository, @Lazy ICustomerService customerService) {
         this.investorProfileRepository = investorProfileRepository;
         this.customerService = customerService;
     }
@@ -26,13 +29,6 @@ public class InvestorProfileService implements IInvestorProfileService {
         investorProfileRepository.save(investorProfile);
     }
 
-    @Override
-    @Transactional
-    public InvestorProfile initBaseInvestorProfile() {
-        InvestorProfile investorProfile = new InvestorProfile();
-        save(investorProfile);
-        return investorProfile;
-    }
 
     @Override
     public InvestorProfile getInvestorProfileByUsername(String username) {
@@ -43,4 +39,72 @@ public class InvestorProfileService implements IInvestorProfileService {
         return investorProfile;
     }
 
+    @Override
+    public InvestorProfileDTO getInvestorProfileDTOByUsername(String username) {
+        InvestorProfile investorProfile = getInvestorProfileByUsername(username);
+        return getInvestorProfileDTOByInvestorProfile(investorProfile);
+    }
+
+    @Override
+    public InvestorProfileDTO getInvestorProfileDTOByInvestorProfile(InvestorProfile investorProfile) {
+        return new InvestorProfileDTO(investorProfile.getInvestmentDurationScore(),
+                investorProfile.getWithdrawalSpendingPlanScore(),
+                investorProfile.getInvestmentKnowledgeScore(),
+                investorProfile.getRiskRewardScore(),
+                investorProfile.getOwnedInvestmentsScore(),
+                investorProfile.getInvestmentPersonalityScore(),
+                investorProfile.getCalculatedTimeHorizonScore(),
+                investorProfile.getCalculatedRiskToleranceScore(),
+                investorProfile.getRecommendedPortfolioType()
+        );
+    }
+
+    @Override
+    public PortfolioTypeEnum calculateRecommendedPortfolioType(InvestorProfile portfolio) {
+        if (portfolio.getCalculatedTimeHorizonScore() < 3) {
+            return PortfolioTypeEnum.CONSERVATIVE;
+        }
+        else if (portfolio.getCalculatedTimeHorizonScore() >= 6) {
+            return portfolio.getCalculatedRiskToleranceScore() < 10 ?
+                    PortfolioTypeEnum.MODERATE : PortfolioTypeEnum.AGGRESSIVE;
+        }
+        else {
+            if (portfolio.getCalculatedRiskToleranceScore() < 6) {
+                return PortfolioTypeEnum.CONSERVATIVE;
+            } else if (portfolio.getCalculatedRiskToleranceScore() < 14) {
+                return PortfolioTypeEnum.MODERATE;
+            } else {
+                return PortfolioTypeEnum.AGGRESSIVE;
+            }
+        }
+    }
+
+    @Override
+    @Transactional
+    public InvestorProfile create(InvestorProfileUpdateDTO investorProfileDTO) {
+        InvestorProfile investorProfile = new InvestorProfile();
+        updateInvestorProfileFromDTO(investorProfile, investorProfileDTO);
+        save(investorProfile);
+        return investorProfile;
+    }
+
+    @Override
+    @Transactional
+    public InvestorProfileDTO update(String username, InvestorProfileUpdateDTO investorProfileDTO) {
+        InvestorProfile investorProfile = getInvestorProfileByUsername(username);
+        updateInvestorProfileFromDTO(investorProfile, investorProfileDTO);
+        save(investorProfile);
+        return getInvestorProfileDTOByInvestorProfile(investorProfile);
+    }
+
+    @Override
+    public void updateInvestorProfileFromDTO(InvestorProfile investorProfile, InvestorProfileUpdateDTO investorProfileDTO) {
+        investorProfile.setInvestmentDurationScore(investorProfileDTO.getInvestmentDurationScore());
+        investorProfile.setWithdrawalSpendingPlanScore(investorProfileDTO.getWithdrawalSpendingPlanScore());
+        investorProfile.setInvestmentKnowledgeScore(investorProfileDTO.getInvestmentKnowledgeScore());
+        investorProfile.setRiskRewardScore(investorProfileDTO.getRiskRewardScore());
+        investorProfile.setOwnedInvestmentsScore(investorProfileDTO.getOwnedInvestmentsScore());
+        investorProfile.setInvestmentPersonalityScore(investorProfileDTO.getInvestmentPersonalityScore());
+        investorProfile.setRecommendedPortfolioType(calculateRecommendedPortfolioType(investorProfile));
+    }
 }
