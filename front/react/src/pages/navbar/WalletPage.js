c285d9af51b473cb6385c12862fb66d6a7218b4f
luca <xiongkun778@163.com>
2024-07-29 18:50:27 +0800

    front react:use wallet api to get data

--- front/react/src/pages/navbar/WalletPage.js
+++ front/react/src/pages/navbar/WalletPage.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const WalletPage = () => {
+    const [data, setData] = useState(null);
+
+    const handleButtonClick = async () => {
+        try {
+            const response = await fetch(' http://localhost:8080/api/v1/customer/wallet');
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            const data = await response.json();
+            setData(data);
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
+    };
+
     return (
         <div>
             <h2>Wallet</h2>
             <p>Welcome to the Wallet page! For Test Only</p>
+            <button onClick={handleButtonClick}>Fetch Data</button>
+            {data && (
+                <div>
+                    <h3>data:</h3>
+                    <pre>{JSON.stringify(data, null, 2)}</pre>
+                </div>
+            )}
         </div>
     );
 };
