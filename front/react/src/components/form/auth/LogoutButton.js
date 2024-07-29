e81e4947b03b862612f824b1ab7c727a1c5512ac
Arabasta <keiyamx@gmail.com>
2024-07-29 21:30:24 +0800

    front react: added logout button component

---
+++ front/react/src/components/form/auth/LogoutButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+import authenticationService from '../../../services/auth/AuthenticationService';
+import useAuth from "../../../hooks/useAuth";
+
+const LogoutComponent = () => {
+    const { setUser } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        authenticationService.logout();
+        setUser(null);
+        navigate('/login');
+    };
+
+    return (
+        <button onClick={handleLogout}>Logout</button>
+    );
+};
+
+export default LogoutComponent;
