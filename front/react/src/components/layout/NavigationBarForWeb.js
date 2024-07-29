adbeeb744ffb32091b2d104bf7f19ae6fcfc2616
luca <xiongkun778@163.com>
2024-07-29 10:33:08 +0800

    front react: reset the nav buttons

--- front/react/src/components/layout/NavigationBarForWeb.js
+++ front/react/src/components/layout/NavigationBarForWeb.js
@@ -1,32 +1,13 @@
-import { Link } from "react-router-dom";
-import logo from "../../assets/images/logo.svg";
+import React from "react";
+import NavButton from "../../components/common/buttons/NavButtons";
 
-export default function NavigationBarForWeb() {
+export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark text-white">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">
-                    <img src={logo} alt="Logo" className="logo" style={{ height: '40px' }} />
-                </a>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/news">NEWS</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/wallet">WALLET</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/support">SUPPORT</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/settings">SETTINGS</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <ul className="navbar-nav">
+            <NavButton to="/news" label="NEWS" />
+            <NavButton to="/settings" label="SETTINGS" />
+            <NavButton to="/support" label="SUPPORT" />
+            <NavButton to="/wallet" label="WALLET" />
+        </ul>
     );
 }
-
-
