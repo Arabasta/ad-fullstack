18da7fc07619bfa1d99f31a899026d8a972fd900
Arabasta <keiyamx@gmail.com>
2024-07-31 00:25:53 +0800

    back spring: add DevAwscontroller and S3Wipe class for wiping transaction bucket

--- back/spring/src/main/java/com/robotrader/spring/security/ExcludedPathsEnum.java
+++ back/spring/src/main/java/com/robotrader/spring/security/ExcludedPathsEnum.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ExcludedPathsEnum {
     HEALTH("/api/v1/health"),
     LOGIN("/api/v1/auth/login"),
-    REGISTER("/api/v1/auth/register");
+    REGISTER("/api/v1/auth/register"),
+    DEVAWSWIPETX("/api/v1/devAws/wipe-tx");
 
     private final String path;
 }
