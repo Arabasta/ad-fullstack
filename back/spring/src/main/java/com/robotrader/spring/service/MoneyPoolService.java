3e9f25d4d14d653cf6b271dfca7667c6a5956b19
Jefri <jefritoo@gmail.com>
2024-07-31 00:03:28 +0800

    back spring trading: change folder for moneypool interface

--- back/spring/src/main/java/com/robotrader/spring/service/MoneyPoolService.java
+++ back/spring/src/main/java/com/robotrader/spring/service/MoneyPoolService.java
@@ -1,13 +1,11 @@
 package com.robotrader.spring.service;
 
-import com.robotrader.spring.interfaces.IMoneyPoolService;
+import com.robotrader.spring.service.interfaces.IMoneyPoolService;
 import com.robotrader.spring.model.MoneyPool;
 import com.robotrader.spring.model.enums.PortfolioTypeEnum;
 import com.robotrader.spring.repository.MoneyPoolRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
 
 @Service
 public class MoneyPoolService implements IMoneyPoolService {
