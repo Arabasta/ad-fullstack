9669694a60db801a1e1d243887cdfc10f815ae53
Arabasta <keiyamx@gmail.com>
2024-07-31 00:23:39 +0800

    back spring: add findUserByPortfolioId to UserRepository

--- back/spring/src/main/java/com/robotrader/spring/repository/UserRepository.java
+++ back/spring/src/main/java/com/robotrader/spring/repository/UserRepository.java
@@ -2,7 +2,15 @@ package com.robotrader.spring.repository;
 
 import com.robotrader.spring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    @Query("SELECT u FROM User u " +
+            "JOIN u.customer c " +
+            "JOIN c.portfolios p " +
+            "WHERE p.id = :portfolioId")
+    User findByPortfolioId(@Param("portfolioId") Long portfolioId);
 }
--- back/spring/src/main/java/com/robotrader/spring/service/UserService.java
+++ back/spring/src/main/java/com/robotrader/spring/service/UserService.java
@@ -7,6 +7,7 @@ import com.robotrader.spring.dto.user.UpdatePasswordDTO;
 import com.robotrader.spring.dto.user.UserCredentialsDTO;
 import com.robotrader.spring.exception.notFound.UserNotFoundException;
 import com.robotrader.spring.model.Customer;
+import com.robotrader.spring.model.Portfolio;
 import com.robotrader.spring.model.User;
 import com.robotrader.spring.model.enums.RoleEnum;
 import com.robotrader.spring.repository.UserRepository;
@@ -133,5 +134,14 @@ public class UserService implements IUserService, UserDetailsService {
     public void lockAccount(String username) {
         User user = getUserByUsername(username);
         user.setRole(RoleEnum.ROLE_LOCKED);
+    }
+
+    @Override
+    public User getUserByPortfolio(Portfolio portfolio) {
+        User user = userRepository.findByPortfolioId(portfolio.getId());
+        if (user == null) {
+            throw new UserNotFoundException("User not found");
+        }
+        return user;
     }
 }
