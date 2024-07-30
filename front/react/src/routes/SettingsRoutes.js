4978a3bdaa40eda4d0be61f1e661da2b8d13426c
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-07-30 22:33:29 +0800

    ADD: AddressPage.js into SettingRoutes

--- front/react/src/routes/SettingsRoutes.js
+++ front/react/src/routes/SettingsRoutes.js
@@ -3,11 +3,13 @@ import AccountPage from "../pages/settings/AccountPage";
 import FinancialProfilePage from "../pages/settings/FinancialProfilePage";
 import NotificationPage from "../pages/settings/NotificationPage";
 import PreferenceFormPage from "../pages/settings/PreferenceFormPage";
+import AddressPage from "../pages/settings/AddressPage";
 import {Route} from "react-router-dom";
 
 
 const settingsRoutes = [
     <Route key="account" path="/settings/account" element={<AccountPage />} />,
+    <Route key="address" path="/settings/address" element={<AddressPage />} />,
     <Route key="financial" path="/settings/financialProfile" element={<FinancialProfilePage />} />,
     <Route key="preference" path="/settings/preferenceForm" element={<PreferenceFormPage />} />,
     <Route key="notification" path="/settings/notification" element={<NotificationPage />} />,
