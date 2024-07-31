05907e95428f77edccc3c827670eda24be782d61
luca <xiongkun778@163.com>
2024-07-31 11:04:01 +0800

    frontend: make InvestorProfileForm to be a component

--- front/react/src/components/form/auth/RegisterStep5.js
+++ front/react/src/components/form/auth/RegisterStep5.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import FormInput from "../../common/inputs/FormInput_Register5";
+import InvestorProfileForm from "../InvestorProfileForm";
 
 const RegisterStep5 = ({
                            investmentDurationScore, setInvestmentDurationScore,
@@ -11,59 +11,26 @@ const RegisterStep5 = ({
                            handlePrevious, handleRegister, message
                        }) => {
     return (
-        <form onSubmit={handleRegister}>
-            <FormInput
-                label="Investment Duration Score"
-                value={investmentDurationScore}
-                onChange={setInvestmentDurationScore}
-                min="1"
-                max="4"
-                required
-            />
-            <FormInput
-                label="Withdrawal Spending Plan Score"
-                value={withdrawalSpendingPlanScore}
-                onChange={setWithdrawalSpendingPlanScore}
-                min="1"
-                max="4"
-                required
-            />
-            <FormInput
-                label="Investment Knowledge Score"
-                value={investmentKnowledgeScore}
-                onChange={setInvestmentKnowledgeScore}
-                min="1"
-                max="4"
-                required
-            />
-            <FormInput
-                label="Risk Reward Score"
-                value={riskRewardScore}
-                onChange={setRiskRewardScore}
-                min="1"
-                max="3"
-                required
-            />
-            <FormInput
-                label="Owned Investments Score"
-                value={ownedInvestmentsScore}
-                onChange={setOwnedInvestmentsScore}
-                min="1"
-                max="3"
-                required
-            />
-            <FormInput
-                label="Investment Personality Score"
-                value={investmentPersonalityScore}
-                onChange={setInvestmentPersonalityScore}
-                min="1"
-                max="4"
-                required
+        <div>
+            <InvestorProfileForm
+                investmentDurationScore={investmentDurationScore}
+                setInvestmentDurationScore={setInvestmentDurationScore}
+                withdrawalSpendingPlanScore={withdrawalSpendingPlanScore}
+                setWithdrawalSpendingPlanScore={setWithdrawalSpendingPlanScore}
+                investmentKnowledgeScore={investmentKnowledgeScore}
+                setInvestmentKnowledgeScore={setInvestmentKnowledgeScore}
+                riskRewardScore={riskRewardScore}
+                setRiskRewardScore={setRiskRewardScore}
+                ownedInvestmentsScore={ownedInvestmentsScore}
+                setOwnedInvestmentsScore={setOwnedInvestmentsScore}
+                investmentPersonalityScore={investmentPersonalityScore}
+                setInvestmentPersonalityScore={setInvestmentPersonalityScore}
+                handleSubmit={handleRegister}
+                buttonText="Register"
+                message={message}
             />
             <button type="button" onClick={handlePrevious}>Previous</button>
-            <button type="submit">Register</button>
-            {message && <p>{message}</p>}
-        </form>
+        </div>
     );
 };
 
