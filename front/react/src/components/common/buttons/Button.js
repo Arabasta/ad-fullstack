0f06e04676440132199196ffe3b5e76bb298c18f
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-07-31 18:08:20 +0800

    ADD: button react component

---
+++ front/react/src/components/common/buttons/Button.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button as Button } from '@chakra-ui/react';
+
+/* Buttons Examples:
+Login, Logout, Register,
+Previous, Next, Last
+*/
+
+const Button = ({ variant, size, type, colorScheme, onClick, children, ...props }) => {
+    return (
+        <Button variant={variant}
+                size={size}
+                type={type}
+                colorScheme={colorScheme}
+                onClick={onClick}
+                {...props}>
+            {children}
+        </Button>
+    );
+};
+
+Button.propTypes = {
+    variant: PropTypes.oneOf(['solid', 'outline', 'ghost', 'link']),
+    size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg']),
+    type: PropTypes.oneOf(['button', 'submit', 'reset']),
+    colorScheme: PropTypes.oneOf([]), // todo: include color scheme
+    onClick: PropTypes.func,
+    children: PropTypes.node.isRequired,
+};
+
+Button.defaultProps = {
+    variant:'solid',
+    // include default colorScheme
+    size:'sm',
+    type: 'button',
+    onClick: () => {},
+};
+
+export default Button;
+
+
