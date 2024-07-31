604e8e379bba330aeb2d4a03bd449b3d4b8ffb5b
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-07-31 18:15:51 +0800

    CHORE: change file paths for SettingsButtons (shift to NavLinks first)

--- front/react/src/pages/navbar/SettingsPage.js
+++ front/react/src/pages/navbar/SettingsPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SettingsButton from "../../components/common/buttons/SettingsButtons";
+import SettingsButton from "../../components/common/navLinks/SettingsButtons";
 
 export default function SettingsPage() {
     return (
