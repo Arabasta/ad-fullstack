d981830e541f4def89e18f311ae7db4dcf80233e
Arabasta <keiyamx@gmail.com>
2024-08-01 00:36:32 +0800

    front react: portfolio transaction history page

--- front/react/src/hooks/useTransactionHistory.js
+++ front/react/src/hooks/useTransactionHistory.js
@@ -37,7 +37,7 @@ const useTransactionHistory = (type, portfolioType = null) => {
                 setLoading(false);
             }
         }, 3500), // throttle delay
-        [loading, hasMore, page, type, portfolioType, size]
+        [loading, hasMore, page, type, portfolioType] // todo: temp removed size need to double check
     );
 
     useEffect(() => {
