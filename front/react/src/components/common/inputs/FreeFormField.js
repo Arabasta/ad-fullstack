53a1e9c2f05a1dbc6b1931d6edf03380914f6f04
xlpy <xenalimpinying@gmail.com>
2024-07-31 19:11:33 +0800

    add FreeFormField.js, SelectField.js

--- front/react/src/components/common/inputs/FreeFormField.js
+++ front/react/src/components/common/inputs/FreeFormField.js
@@ -1,19 +1,38 @@
 import React from 'react';
 
-const FreeformField = ({ label, value, onChange, min, max, required }) => {
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FreeFormField = ({ label, type, value, onChange, placeholder, required }) => {
     return (
         <div>
-            <label>{label}</label>
+            {label && <label>{label}</label>}
             <input
-                type="number"
+                type={type}
                 value={value}
-                onChange={(e) => onChange(parseInt(e.target.value))}
-                min={min}
-                max={max}
+                onChange={onChange}
+                placeholder={placeholder}
                 required={required}
             />
         </div>
     );
 };
 
-export default FreeformField;
+FreeFormField.propTypes = {
+    label: PropTypes.string,
+    type: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    onChange: PropTypes.func.isRequired,
+    placeholder: PropTypes.string,
+    required: PropTypes.bool,
+};
+
+FreeFormField.defaultProps = {
+    label: '',
+    type: 'text',
+    placeholder: '',
+    required: false,
+};
+
+export default FreeFormField;
+
