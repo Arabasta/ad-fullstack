f56d2698a5d5bd7454655169d8110ef003e9a125
Arabasta <83902652+Arabasta@users.noreply.github.com>
2024-08-02 20:59:42 +0800

    Update index.js

--- front/react/src/index.js
+++ front/react/src/index.js
@@ -30,11 +30,9 @@ const theme = extendTheme({ colors, config });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
       <ChakraProvider theme={theme}>
           <App />
       </ChakraProvider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
