38d51aa6760223cb87ec5fb9e2d17a0eb86bebaf
xlpy <xenalimpinying@gmail.com>
2024-08-03 12:27:17 +0800

    FIX: ProfileButtons.js to use base component

--- front/react/src/components/elements/buttons/ProfileButtons.js
+++ front/react/src/components/elements/buttons/ProfileButtons.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Button, Flex, useColorModeValue } from '@chakra-ui/react';
-
+import Button from './Button'; // Adjust the import path as needed
 
 const ProfileButtons = ({ to, label }) => {
     const navigate = useNavigate();
-    //const [colorCode, setColorCode] = useState(colorList[randomColor()]);
 
     const handleClick = () => {
         navigate(to);
     };
 
     return (
-        <Flex justifyContent="center" alignItems="center" p={4}>
-            <Button
-                px={8}
-                bg={useColorModeValue('#151f21', 'gray.900')}
-                color={'white'}
-                rounded={'md'}
-                _hover={{
-                    bg: useColorModeValue('#1a202c', '#2d3748'),
-                    transform: 'translateY(-2px)',
-                    boxShadow: 'lg',
-                }}
-                onClick={handleClick}
-            >
+        <div>
+            <Button onClick={handleClick}>
                 {label}
             </Button>
-        </Flex>
+        </div>
     );
 };
 
