528ec5ca5decd0fafc6e434aadc9531496c9172b
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-03 22:49:16 +0800

    ADD: helpers date functions

--- front/react/src/utils/helpers.js
+++ front/react/src/utils/helpers.js
@@ -1,1 +1,9 @@
-//
+export const isInToday = (inputDate) => {
+    const today = new Date();
+    return today.setHours(0, 0, 0, 0) === inputDate.setHours(0, 0, 0, 0);
+};
+
+export const isInYesterday = (inputDate) => {
+    const yestDay = new Date(Date.now() - 24 * 60 * 60 * 1000);
+    return yestDay.setHours(0, 0, 0, 0) === inputDate.setHours(0, 0, 0, 0);
+};
