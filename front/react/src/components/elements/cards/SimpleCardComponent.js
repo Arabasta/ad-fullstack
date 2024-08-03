6fc37e4a38dd07688ba467e6fc1fad2abdf17052
xlpy <xenalimpinying@gmail.com>
2024-08-03 13:30:56 +0800

    ADD: SimpleCardComponent.js and CustomerDetailsCard.js

--- front/react/src/components/elements/cards/SimpleCardComponent.js
+++ front/react/src/components/elements/cards/SimpleCardComponent.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Box, Text, VStack } from '@chakra-ui/react';
 
 const SimpleCardComponent = ({ title, subtitle, description }) => {
     return (
-        <Box
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
-            boxShadow="md"
-            p={6}
-            bg="gray.800"
-            color="white"
-        >
-            <VStack spacing={4} align="stretch">
-                <Text fontSize="lg" fontWeight="bold">
-                    {title}
-                </Text>
-                {subtitle && (
-                    <Text fontSize="md">
-                        {subtitle}
-                    </Text>
-                )}
-                <Text fontSize="sm">
-                    {description}
-                </Text>
-            </VStack>
-        </Box>
+        <div>
+            <p>{title}</p>
+            <p>{subtitle}</p>
+            <p>{description}</p>
+        </div>
     );
 };
 
