8a6103c85b550677fb65a5ae657e92669ecd9956
xlpy <xenalimpinying@gmail.com>
2024-08-03 14:13:37 +0800

    ADD: chakra styling for SimpleCardComponent.js and CustomerDetailsCard.js

--- front/react/src/components/elements/cards/SimpleCardComponent.js
+++ front/react/src/components/elements/cards/SimpleCardComponent.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Box, Text, VStack } from '@chakra-ui/react';
 
 const SimpleCardComponent = ({ title, subtitle, description }) => {
     return (
-        <div>
-            <p>{title}</p>
-            <p>{subtitle}</p>
-            <p>{description}</p>
-        </div>
+        <Box
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            boxShadow="md"
+            p={6}
+            bg="gray.800"
+            color="white"
+        >
+            <VStack spacing={4} align="stretch">
+                <Text fontSize="lg" fontWeight="bold">
+                    {title}
+                </Text>
+                {subtitle && (
+                    <Text fontSize="md">
+                        {subtitle}
+                    </Text>
+                )}
+                <Text fontSize="sm">
+                    {description}
+                </Text>
+            </VStack>
+        </Box>
     );
 };
 
