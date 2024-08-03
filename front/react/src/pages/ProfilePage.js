13dab80d4fcc298e5719bbae806c8a4f4fea766b
xlpy <xenalimpinying@gmail.com>
2024-08-03 13:30:56 +0800

    ADD: SimpleCardComponent.js and CustomerDetailsCard.js

--- front/react/src/pages/ProfilePage.js
+++ front/react/src/pages/ProfilePage.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from "../components/pageSections/headers/Header";
-import RecommendedPortfolioType from "../components/elements/alerts/info/RecommendedPortfolioType";
+import RecommendedPortfolioType from "../components/elements/alerts/info/RecommendedPortfolioTypeInfoAlert";
 import ProfileButtons from "../components/elements/buttons/ProfileButtons";
 import Footer from "../components/pageSections/footers/Footer";
-import ShowCustomerDetails from "../components/customer/ShowCustomerDetails";
-import useCustomer from "../hooks/useCustomer";
-
+import CustomerDetailsCard from "../components/elements/cards/CustomerDetailsCard";
+import CustomerService from "../services/CustomerService";
 
 const ProfilePage = () => {
+    const [customer, setCustomer] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchCustomerData = async () => {
+            try {
+                const response = await CustomerService.getCustomer()
+                setCustomer(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching customer details:', error);
+                setLoading(false);
+            }
+        };
+
+        fetchCustomerData();
+    }, []);
+
+    if (loading) {
+        return <p>Loading customer details...</p>;
+    }
+
     return (
         <div>
             <Header/>
             <h2>Profile</h2>
             <p>Welcome to the Profile Page!</p>
 
-
+            <CustomerDetailsCard customer={customer} />
 
             <RecommendedPortfolioType/>
             <ul className="nav">
