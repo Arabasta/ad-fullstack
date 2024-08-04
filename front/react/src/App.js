e2c90e10ba356048ed48ed1956513bdc8cd36942
Arabasta <keiyamx@gmail.com>
2024-08-05 07:42:42 +0800

    front react: another another merge conflict

--- front/react/src/App.js
+++ front/react/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Routes } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './config/context/AuthContext';
 import ErrorBoundary from './components/elements/alerts/error/ErrorBoundary';
 import authRoutes from './routes/AuthRoutes';
 import mainRoutes from './routes/MainRoutes';
 import profileRoutes from './routes/ProfileRoutes';
-import NaviRoutes from './routes/NaviRoutes';
 import portfolioRoutes from './routes/PortfolioRoutes';
 import Header from "./components/pageSections/headers/Header";
 import Footer from "./components/pageSections/footers/Footer";
+import NavigationBarRoutes from "./navbar/NavigationBarRoutes";
 
 
 const App = () => {
@@ -28,7 +27,7 @@ const AppContent = () => {
             <Header />
             <ErrorBoundary>
                 <Routes>
-                    {navigationBarRoutesRoutes}
+                    {NavigationBarRoutes}
                     {authRoutes}
                     {mainRoutes}
                     {profileRoutes}
