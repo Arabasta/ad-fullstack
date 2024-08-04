8a2e8339f5623506c168ebd25bdf81c77bb3697c
luca <xiongkun778@163.com>
2024-08-04 22:18:35 +0800

    frontend: fix CI/bulid checks

--- front/react/src/hooks/usePortfolio.js
+++ front/react/src/hooks/usePortfolio.js
@@ -4,14 +4,18 @@ import PortfolioService from '../services/PortfolioService';
 const usePortfolio = (portfolioType) => {
     const [portfolio, setPortfolio] = useState({});
 
-    const getPortfolio = async () => {
-        try {
-            const response = await PortfolioService.getPortfolio(portfolioType);
-            setPortfolio(response.data.data);
-        } catch (error) {
-            console.error('Error fetching portfolio data', error);
-        }
-    };
+    useEffect(() => {
+        const getPortfolio = async () => {
+            try {
+                const response = await PortfolioService.getPortfolio(portfolioType);
+                setPortfolio(response.data.data);
+            } catch (error) {
+                console.error('Error fetching portfolio data', error);
+            }
+        };
+
+        getPortfolio();
+    }, [portfolioType]);
 
     const addFunds = async (amount) => {
         try {
@@ -30,10 +34,6 @@ const usePortfolio = (portfolioType) => {
             console.error('Error withdrawing funds', error);
         }
     };
-
-    useEffect(() => {
-        getPortfolio();
-    }, [portfolioType]);
 
     return { portfolio, addFunds, withdrawFunds };
 };
