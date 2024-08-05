b1a43fd52040f6034c272895d238012ca0d68acd
luca <xiongkun778@163.com>
2024-08-05 15:31:08 +0800

    frontend: fix ci  v2.0

--- front/react/src/admin/hooks/useUsers.js
+++ front/react/src/admin/hooks/useUsers.js
@@ -1,21 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import ManageUsersService from "../services/ManageUsersService";
 
 const useUsers = (search = '') => {
     const [users, setUsers] = useState([]);
 
-    const getUsers = async () => {
+    const getUsers = useCallback(async () => {
         try {
             const response = await ManageUsersService.getUsers(search);
             setUsers(response.data.data);
         } catch (error) {
             console.error('Error fetching users data', error);
         }
-    };
+    }, [search]);
 
     useEffect(() => {
         getUsers();
-    }, [search,getUsers]);
+    }, [getUsers]);
 
     return { users, getUsers };
 };
