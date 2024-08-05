6fbc6049550e84fab14c6810bd19b2b8613cf423
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-05 18:08:14 +0800

    REFACTOR: consumptions of APIs

--- front/react/src/admin/services/LiveTradingService.js
+++ front/react/src/admin/services/LiveTradingService.js
@@ -1,15 +1,17 @@
 import axiosInstance from "../../config/axios/axiosInstance";
 
-const startTrade = async () => {
-    return await axiosInstance.get(`/v1/admin/trading/livetrading/start`);
+const startTrade = async (portfolioType = '', tickerType='') => {
+    const url = `/v1/admin/trading/livetrading/start?portfolioType=${portfolioType}&tickerType=${tickerType}`
+    return await axiosInstance.get(url);
 };
 
 const stopTrade = async () => {
     return await axiosInstance.get(`/v1/admin/trading/livetrading/stop`);
 };
 
-const seeTransactions = async () => {
-    return await axiosInstance.get(`/v1/admin/trading/livetrading/transactions`);
+const seeTransactions = async (portfolioType = '', page = 0, size= 10 ) => {
+    const url = `/api/v1/admin/trading/livetrading/transactions?portfolioType=${portfolioType}&page=${page}&size=${size}`;
+    return await axiosInstance.get(url);
 };
 
 const LiveTradingService = {
