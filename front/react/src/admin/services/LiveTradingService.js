7caae08258619c752163738f4598fbcb7128b79f
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-05 16:23:42 +0800

    ADD: LiveTradingService

---
+++ front/react/src/admin/services/LiveTradingService.js
@@ -0,0 +1,21 @@
+import axiosInstance from '../config/axios/axiosInstance';
+
+const startTrade = async () => {
+    return await axiosInstance.get(`/v1/admin/trading/livetrading/start`);
+};
+
+const stopTrade = async () => {
+    return await axiosInstance.get(`/v1/admin/trading/livetrading/stop`);
+};
+
+const seeTransactions = async () => {
+    return await axiosInstance.get(`/v1/admin/trading/livetrading/transactions`, { amount });
+};
+
+const LiveTradingService = {
+    startTrade,
+    stopTrade,
+    seeTransactions,
+};
+
+export default LiveTradingService;
