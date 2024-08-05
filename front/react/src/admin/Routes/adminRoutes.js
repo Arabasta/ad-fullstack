a1684874c2e9aaa09b1ddc3208f036fde6f3ed3a
xlpy <xenalimpinying@gmail.com>
2024-08-05 19:20:57 +0800

    add routing for ManageTickersPage.js

--- front/react/src/admin/Routes/adminRoutes.js
+++ front/react/src/admin/Routes/adminRoutes.js
@@ -7,6 +7,8 @@ import AdminManageLiveTradingPage from "../pages/AdminManageLiveTradingPage";
 
 const adminRoutes = [
     <Route key="account" path="/admin/manage-user" element={<AdminViewUsersPage />} />,
+    <Route key="manage-tickers" path="/manage-tickers" element={<ManageTickersPage />} />
+
     <Route key="backTest" path="/admin/backtest" element={<BackTestPage />} />,
     <Route key="manageLiveTrading" path="/admin/managelivetrading" element={<AdminManageLiveTradingPage />} />,
     <Route path="/admin/backtest-result" element={<BackTestResultPage />} />
---
+++ front/react/src/admin/pages/ManageTickersPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManageTickersPage = () => {
+    return (
+        <div>
+            <h2>Manage Tickers Page</h2>
+            <p>Welcome to the Manage Tickers page! For Test Only</p>
+        </div>
+    );
+};
+
+export default ManageTickersPage;
