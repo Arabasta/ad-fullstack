43bf8cc191f6705f1ea92f5f718694b874020690
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-05 14:53:20 +0800

    FIX: wrap getRulesByPortfolio in callback and robust validation for data received

--- front/react/src/hooks/useRulesByPortfolio.js
+++ front/react/src/hooks/useRulesByPortfolio.js
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect, useCallback} from 'react';
 import RulesService from "../services/RulesService";
 
-const useRulesByPortfolio = (portfolioType=null) => {
+const useRulesByPortfolio = (portfolioType) => {
+
     const [rules, setRules] = useState({
         portfolioType: portfolioType,
         stopLossInitialValue: 0.0,
@@ -10,27 +11,31 @@ const useRulesByPortfolio = (portfolioType=null) => {
         recurringAllocationDay: 0,
     });
 
-    const getRulesByPortfolio = async () => {
+    const getRulesByPortfolio = useCallback(async () => {
         try {
             const response = await RulesService.getRulesByPortfolioType(portfolioType);
-            const data = response.data.data;
-            setRules({
-                portfolioType: data.portfolioType,
-                stopLossInitialValue: data.stopLossInitialValue,
-                stopLoss: data.stopLoss,
-                recurringAllocationAmount: data.recurringAllocationAmount,
-                recurringAllocationDay: data.recurringAllocationDay,
-            });
+            const data = response?.data?.data;
+            if (data) {
+                setRules({
+                    portfolioType: data.portfolioType,
+                    stopLossInitialValue: data.stopLossInitialValue,
+                    stopLoss: data.stopLoss,
+                    recurringAllocationAmount: data.recurringAllocationAmount,
+                    recurringAllocationDay: data.recurringAllocationDay,
+                });
+            } else {
+                console.error('No data found in response');
+            }
         } catch (error) {
             console.error('Error fetching rules data', error);
         }
-    };
+    },[portfolioType]);
 
     useEffect(() => {
         getRulesByPortfolio();
-    }, [portfolioType]);
+    }, [getRulesByPortfolio]);
 
     return { rules, getRulesByPortfolio };
 };
 
-export default useRulesByPortfolio();
+export default useRulesByPortfolio;
