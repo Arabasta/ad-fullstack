a1536ac3e2c91e1415f671666733c88242756621
zephyrdark <zephyrdark@users.noreply.github.com>
2024-08-05 18:47:22 +0800

    back-spring-trading-predictions: add predict by tickerDTO by calling fastapi backend

--- back/spring/src/main/java/com/robotrader/spring/trading/service/PredictionService.java
+++ back/spring/src/main/java/com/robotrader/spring/trading/service/PredictionService.java
@@ -9,8 +9,15 @@ import com.robotrader.spring.exception.aws.TransactionRetrievalException;
 import com.robotrader.spring.model.enums.TickerTypeEnum;
 import com.robotrader.spring.trading.dto.PredictionDTO;
 import com.robotrader.spring.trading.dto.PredictionDTOListDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -18,6 +25,7 @@ import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -27,21 +35,58 @@ public class PredictionService {
 
     private final String AWS_S3_PREDICTION_BUCKET_NAME;
     private final String AWS_S3_MODEL_BUCKET_NAME;
+    private final WebClient fastapiWebClient;
+    private static final Logger logger = LoggerFactory.getLogger(PredictionService.class);
 
     public PredictionService(@Value("${AWS_S3_PREDICTION_BUCKET_NAME}") String awsS3PredictionBucketName,
-                             @Value("${AWS_S3_MODEL_BUCKET_NAME}") String awsS3ModelBucketName) {
+                             @Value("${AWS_S3_MODEL_BUCKET_NAME}") String awsS3ModelBucketName,
+                             @Value("${BACK_FASTAPI_URL}") String backFastapiUrl,
+                             WebClient.Builder webClientBuilder) {
         this.AWS_S3_PREDICTION_BUCKET_NAME = awsS3PredictionBucketName;
         this.AWS_S3_MODEL_BUCKET_NAME = awsS3ModelBucketName;
+        this.fastapiWebClient = webClientBuilder
+                .baseUrl(backFastapiUrl)
+                .build();
     }
 
-    public PredictionDTO byTicker(TickerDTO tickerDTO) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
+    private PredictionDTO byPredictionDtoBacktest(PredictionDTO predictionDTO) {
+        // todo: priority2
+        // backtest service will call this to pass historical prices to fastapi to perform prediction
+        // 1. Build PredictionDTO into json
+        // 2. Send json via HTTP Request to fastapi backend api
+        // 3. Wait for predictions, and return
+        return null;
+    }
+
+    public PredictionDTO byTickerDtoLive(TickerDTO tickerDTO) throws IOException {
+        // todo: priority1
+        // 1. Build TickerDTO into json
+        // 2. Send json via HTTP Request to fastapi backend api
+        // 3. Wait for predictions, and return
+        String api = "/api/v1/predict/ticker/live";
         String tickerName = tickerDTO.getTickerName().toUpperCase();
+        Mono<PredictionDTO> stream = fastapiWebClient.post()
+                .uri(api)
+                .bodyValue(tickerDTO)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, response -> {
+                    logger.error("4xx error occurred for ticker: {}", tickerName);
+                    return Mono.error(new RuntimeException("4xx error"));
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, response -> {
+                    logger.error("5xx error occurred for ticker: {}", tickerName);
+                    return Mono.error(new RuntimeException("5xx error"));
+                })
+                .bodyToMono(PredictionDTO.class);
         // todo: call fastApi for updated predictions, instead of reading static predictions .json from s3.
-        return mapper.readValue(readJsonFromS3Bucket(tickerName), PredictionDTO.class);
+        return stream.block();
     }
 
-    public PredictionDTOListDTO byTickerList(TickerDTOListDTO tickerDTOListDTO) throws IOException {
+    // todo: low priority, since trading side are using individual TickerDTOs, not list.
+    public PredictionDTOListDTO byTickerDtoListDtoLive(TickerDTOListDTO tickerDTOListDTO) throws IOException {
+        // 1. Build TickerDTOListDTO into json
+        // 2. Send json via HTTP Request to fastapi backend api
+        // 3. Wait for predictions, and return
         List<TickerDTO> tickerDTOList = tickerDTOListDTO.getTickerDTOList();
         if (tickerDTOList.isEmpty()) {
             throw new IOException("Ticker list is empty");
@@ -49,7 +94,7 @@ public class PredictionService {
         List<PredictionDTO> list = tickerDTOList.stream()
                 .map(tickerDTO -> {
                     try {
-                        return byTicker(tickerDTO);
+                        return byTickerDtoLive(tickerDTO);
                     } catch (IOException e) {
                         throw new RuntimeException("Failed to fetch prediction for ticker: " + tickerDTO.getTickerName(), e);
                     }
@@ -75,7 +120,7 @@ public class PredictionService {
         // Transform the list of stock names into TickerDTO and collect into TickerDTOListDTO
         List<TickerDTO> tickerDTOList = logger
                 .retrieveObjectNameList(AWS_S3_MODEL_BUCKET_NAME).stream()
-                .map(fileName -> fileName.split("\\.")[0]) // splits AAPL.json to AAPL
+                .map(fileName -> fileName.split("\\.")[0]) // e.g., splits 'AAPL.pkl' and returns 'AAPL'
                 .map(tickerName -> new TickerDTO(tickerTypeEnum, tickerName))
                 .toList();
 
