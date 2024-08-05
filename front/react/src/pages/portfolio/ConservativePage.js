290ceb1c879e2dab398b35e679b9a2876e8a5948
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-05 14:56:16 +0800

    TEST: RulePage in one of the portfolio page

--- front/react/src/pages/portfolio/ConservativePage.js
+++ front/react/src/pages/portfolio/ConservativePage.js
@@ -3,6 +3,7 @@ import usePortfolio from "../../hooks/usePortfolio";
 import PortfolioDetails from "../../components/portfolio/PortfolioDetails";
 import PortfolioAddFunds from "../../components/portfolio/PortfolioAddFunds";
 import PortfolioWithdrawFunds from "../../components/portfolio/PortfolioWithdrawFunds";
+import RulesPage from "./rules/RulesPage";
 
 const ConservativePage = () => {
     const { portfolio, addFunds, withdrawFunds } = usePortfolio('CONSERVATIVE');
@@ -13,6 +14,7 @@ const ConservativePage = () => {
             <PortfolioDetails portfolio={portfolio} />
             <PortfolioAddFunds addFunds={addFunds} />
             <PortfolioWithdrawFunds withdrawFunds={withdrawFunds}  currentBalance={portfolio.allocatedBalance}/>
+            <RulesPage portfolio={portfolio.portfolioType}/>
         </div>
     );
 };
