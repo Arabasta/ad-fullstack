60fadca8379b456fcb637f0a82b0a41c07ea5539
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-06 23:39:42 +0800

    ADD: countries and nationalies

---
+++ front/react/src/components/customer/auth/Countries.js
@@ -0,0 +1,6 @@
+const countries = [
+    'America', 'United Kingdom', 'Canada', 'China', 'France', 'German', 'Vietnam',
+    'India', 'Japan', 'Nigeria', 'South Africa', 'Singapore', 'Myanmar' // Add more as needed
+];
+
+export default countries;
