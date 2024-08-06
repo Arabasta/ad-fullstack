d692b8fa997efcdd75f6ea9b6339a06a1304c375
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-06 16:35:12 +0800

    ADD: linechart component to view backtest results

--- front/react/src/admin/pages/BackTestResultPage.js
+++ front/react/src/admin/pages/BackTestResultPage.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import LineChart from "../component/charts/LineChart";
 
 const BackTestResultPage = () => {
     const location = useLocation();
     const { labels, datasets } = location.state;
+
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Capital',
+                data: datasets[0].data,
+                yAxisID: 'y-axis-1',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                tension: 0.4,
+            },
+            {
+                label: 'Percent Change',
+                data: datasets[1].data,
+                yAxisID: 'y-axis-2',
+                borderColor: 'rgba(153, 102, 255, 1)',
+                backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                tension: 0.4,
+            },
+        ],
+    };
 
     return (
         <div>
@@ -24,6 +47,8 @@ const BackTestResultPage = () => {
                     </li>
                 ))}
             </ul>
+
+            <LineChart data={data} />
         </div>
     );
 };
