7506ef17a35b7a4b867c59981a199ad9f05c02f9
luca <xiongkun778@163.com>
2024-08-06 15:19:43 +0800

    frontend: implement api:admin lock/unlock user

--- front/react/src/admin/hooks/useUsers.js
+++ front/react/src/admin/hooks/useUsers.js
@@ -13,11 +13,29 @@ const useUsers = (search = '') => {
         }
     }, [search]);
 
+    const lockUser = useCallback(async (username) => {
+        try {
+            await ManageUsersService.lockUser(username);
+            await getUsers(); // Refresh the users list after locking a user
+        } catch (error) {
+            console.error('Error locking user', error);
+        }
+    }, [getUsers]);
+
+    const unlockUser = useCallback(async (username) => {
+        try {
+            await ManageUsersService.unlockUser(username);
+            await getUsers(); // Refresh the users list after unlocking a user
+        } catch (error) {
+            console.error('Error unlocking user', error);
+        }
+    }, [getUsers]);
+
     useEffect(() => {
         getUsers();
     }, [getUsers]);
 
-    return { users, getUsers };
+    return { users, getUsers, lockUser, unlockUser };
 };
 
 export default useUsers;
