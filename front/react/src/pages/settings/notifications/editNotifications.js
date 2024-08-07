bddbde93f9c7d2100de977c5bd061f0285ee439d
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-07 18:22:26 +0800

    front-end-profile (ADD): placeholder page for notifications

--- front/react/src/pages/settings/notifications/editNotifications.js
+++ front/react/src/pages/settings/notifications/editNotifications.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ProfileButtons from "../../../components/common/buttons/ProfileButtons";
 
 const EditNotificationsPage = () => {
     return (
         <div>
             <h2>Main Edit Notifications</h2>
+            <ProfileButtons to="/profile/notification" label="Notifications" />
         </div>
     );
 };
