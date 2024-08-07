3526b5cd1361753fe5e9a694629fed148790f7ba
xlpy <xenalimpinying@gmail.com>
2024-08-07 09:49:41 +0800

    FIX: SupportPage.js

--- front/react/src/pages/SupportPage.js
+++ front/react/src/pages/SupportPage.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Header from "../components/common/layout/Header";
-import Footer from "../components/common/layout/Footer";
 import Faq from "../components/support/Faqs";
 
 const SupportPage = () => {
