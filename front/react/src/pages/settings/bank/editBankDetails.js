1edf122bba4b97e05ab536d05ae59e15b5b53467
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-07 18:22:15 +0800

    front-end-profile (ADD): placeholder page for bank details

--- front/react/src/pages/settings/bank/editBankDetails.js
+++ front/react/src/pages/settings/bank/editBankDetails.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import ProfileButtons from "../../../components/common/buttons/ProfileButtons";
 
 const EditBankPage = () => {
     return (
         <div>
             <h2>Main Edit Bank</h2>
+            <ProfileButtons to="/profile/bankDetails" label="Bank Details" />
+
         </div>
     );
 };
