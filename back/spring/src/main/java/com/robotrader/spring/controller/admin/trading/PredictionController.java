49c37eebc5d8a5997627ee49938fb1de61d3bc1b
zephyrdark <zephyrdark@users.noreply.github.com>
2024-08-07 12:22:28 +0800

    back-spring-trading-predictions: fix predictionService to include crypto in getAvailableTickers, fix Mono to return as stream only

--- back/spring/src/main/java/com/robotrader/spring/controller/admin/trading/PredictionController.java
+++ back/spring/src/main/java/com/robotrader/spring/controller/admin/trading/PredictionController.java
@@ -4,20 +4,25 @@ import com.robotrader.spring.dto.general.ApiResponse;
 import com.robotrader.spring.dto.ticker.TickerDTO;
 import com.robotrader.spring.dto.ticker.TickerDTOListDTO;
 import com.robotrader.spring.trading.dto.PredictionDTO;
-import com.robotrader.spring.trading.dto.PredictionDTOListDTO;
 import com.robotrader.spring.trading.service.PredictionService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.scheduler.Schedulers;
 
 import java.io.IOException;
 
-//todo: /prediction/ paths used for API debugging. delete before submission if not needed.
+/*
+ * todo: Note: PredictionController is used for API debugging. delete before submission if not needed.
+ */
+
 @RestController
 @RequestMapping("/api/v1/admin/prediction")
 public class PredictionController {
 
     private final PredictionService predictionService;
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(PredictionController.class);
 
     @Autowired
     public PredictionController(PredictionService predictionService) {
@@ -27,25 +32,24 @@ public class PredictionController {
     @GetMapping("available")
     public ResponseEntity<ApiResponse<TickerDTOListDTO>> getAvailableTickers() {
         TickerDTOListDTO tickerDTOListDTO = predictionService.getAvailableTickers();
-        return ResponseEntity.ok(new ApiResponse<>("success", "Available predicted TickerDTOs retrieved successfully", tickerDTOListDTO));
+        return ResponseEntity.ok(new ApiResponse<>("success", "Available TickerDTOs for predictions retrieved successfully", tickerDTOListDTO));
     }
 
     @PostMapping("ticker/backtest")
     public ResponseEntity<ApiResponse<PredictionDTO>> predict(@RequestBody PredictionDTO predictionDTO) throws IOException {
-        PredictionDTO predictionDtoResults = predictionService.byPredictionDtoBacktest(predictionDTO).block();
+        PredictionDTO predictionDtoResults = predictionService.byPredictionDtoBacktest(predictionDTO)
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnError(error -> logger.error("Error fetching backtest predictions: {}{}", error.getMessage(), predictionDTO))
+                .block();
         return ResponseEntity.ok(new ApiResponse<>("success", "Predicted backtest data successfully", predictionDtoResults));
     }
 
     @PostMapping("ticker/live")
     public ResponseEntity<ApiResponse<PredictionDTO>> predict(@RequestBody TickerDTO tickerDTO) throws IOException {
-        PredictionDTO predictionDTO = predictionService.byTickerDtoLive(tickerDTO).block();
+        PredictionDTO predictionDTO = predictionService.byTickerDtoLive(tickerDTO)
+                .subscribeOn(Schedulers.boundedElastic())
+                .doOnError(error -> logger.error("Error fetching live predictions: {}{}", error.getMessage(), tickerDTO))
+                .block();
         return ResponseEntity.ok(new ApiResponse<>("success", "Predicted TickerDTO successfully", predictionDTO));
     }
-
-//    // todo: not tested. to see if needed.
-//    @PostMapping("ticker_list/live")
-//    public ResponseEntity<ApiResponse<PredictionDTOListDTO>> predict(@RequestBody TickerDTOListDTO tickerDTOListDTO) throws IOException {
-//        PredictionDTOListDTO predictionDTOListDTO = predictionService.byTickerDtoListDtoLive(tickerDTOListDTO);
-//        return ResponseEntity.ok(new ApiResponse<>("success", "Predicted TickerDTOList successfully", predictionDTOListDTO));
-//    }
 }
