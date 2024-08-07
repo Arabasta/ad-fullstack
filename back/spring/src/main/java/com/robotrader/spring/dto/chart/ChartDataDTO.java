85e1682debb2ef651fd4c395f6a5d2aef1c9eff5
Jefri <jefritoo@gmail.com>
2024-08-08 00:43:59 +0800

    back spring: add chart service method to transform portfolio history into ChartDataDTO

--- back/spring/src/main/java/com/robotrader/spring/dto/chart/ChartDataDTO.java
+++ back/spring/src/main/java/com/robotrader/spring/dto/chart/ChartDataDTO.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 public class ChartDataDTO {
-    private List<Integer> labels;
+    private List<?> labels;
     private List<ChartDatasetDTO> datasets;
 }
 
