45dde7642546e24439075050a99e76b1564c4919
xlpy <xenalimpinying@gmail.com>
2024-08-09 23:00:22 +0800

    FIX: AdminViewUsersPage.js

--- front/react/src/admin/pages/AdminViewUsersPage.js
+++ front/react/src/admin/pages/AdminViewUsersPage.js
@@ -5,7 +5,7 @@ import UserList from '../component/UserList';
 const AdminPage = () => {
     return (
         <Container maxW="container.lg" p={4}>
-            <Heading as="h1" size="lg" mb={6}>
+            <Heading as="h1" size="lg" mb={6} textAlign="center">
                 Manage Users
             </Heading>
             <UserList />
