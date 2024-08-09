72d19f893297765f086e3183c23c3111bc9365bd
Jefri <jefritoo@gmail.com>
2024-08-09 22:36:38 +0800

    back spring: add annotations for trade transaction log entity

--- back/spring/src/main/java/com/robotrader/spring/model/log/TradeTransactionLog.java
+++ back/spring/src/main/java/com/robotrader/spring/model/log/TradeTransactionLog.java
@@ -2,6 +2,8 @@ package com.robotrader.spring.model.log;
 
 import com.robotrader.spring.model.enums.PortfolioTypeEnum;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,12 +20,33 @@ import java.time.LocalDateTime;
 public class TradeTransactionLog {
     @Id
     private String transactionId;
+
+    @NotNull(message = "Ticker cannot be null")
+    @Column(nullable = false)
     private String ticker;
+
+    @NotNull(message = "Action cannot be null")
+    @Column(nullable = false)
     private String action;
+
+    @NotNull(message = "Timestamp cannot be null")
     private LocalDateTime transactionDateTime;
+
+    @NotNull(message = "Transaction quantity cannot be null")
+    @DecimalMin(value = "0", message = "Amount must be greater than or equal to 0")
+    @Column(nullable = false, precision = 14, scale = 2)
     private BigDecimal transactionQuantity;
+
+    @NotNull(message = "Transaction price cannot be null")
+    @DecimalMin(value = "0", message = "Amount must be greater than or equal to 0")
+    @Column(nullable = false, precision = 16, scale = 4)
     private BigDecimal transactionPrice;
+
+    @NotNull(message = "Transaction amount cannot be null")
+    @DecimalMin(value = "0", message = "Amount must be greater than or equal to 0")
+    @Column(nullable = false, precision = 16, scale = 4)
     private BigDecimal transactionAmount;
+
     @Enumerated(EnumType.STRING)
     private PortfolioTypeEnum portfolioType;
 }
