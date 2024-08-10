c8dcef11e1b88c798fc81b6d1aef977a507f5f99
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-10 16:20:27 +0800

    front-end-porfolio-charts (ADD): format timestamps for x-axis

--- front/react/src/admin/pages/BackTestResultPage.js
+++ front/react/src/admin/pages/BackTestResultPage.js
@@ -16,8 +16,28 @@ const BackTestResultPage = () => {
         setView(view === 'capital' ? 'percentChange' : 'capital');
     };
 
+    function formatLabels(labels) {
+        return labels.map(label => {
+            const [year, month, day, hour, minute] = label;
+            const date = new Date(year, month - 1, day, hour, minute);
+
+            // Options for the formatting
+            const options = {
+                year: 'numeric',
+                month: 'short',  // 'short' for abbreviated month name, 'long' for full name
+                day: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: true, // true for 12-hour time, false for 24-hour time
+            };
+
+            return date.toLocaleString('en-SG', options); // Adjust format as needed
+        });
+    }
+    const formattedLabels = formatLabels(labels);
+
     const data = {
-        labels: labels,
+        labels: formattedLabels,
         datasets: view === 'capital' ? [
                 {
                     label: 'Capital',
