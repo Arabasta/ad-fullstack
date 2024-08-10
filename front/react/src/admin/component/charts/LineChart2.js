6411e2fa4a5f6b0eb5775cc49118dd2ec32d32b8
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-10 16:21:23 +0800

    front-end-porfolio-charts (ADD): LineChart component for portfolio value charts

--- front/react/src/admin/component/charts/LineChart2.js
+++ front/react/src/admin/component/charts/LineChart2.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend } from 'chart.js';
@@ -50,3 +51,104 @@ const LineChart2 = ({ data, labels }) => {
 };
 
 export default LineChart2;
+*/
+
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip,
+    Legend,
+} from "chart.js";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    LineElement,
+    PointElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+const LineChart2 = ({ labels, datasets, view }) => {
+    const data = {
+        labels,
+        datasets: datasets.map((dataset, index) => ({
+            ...dataset,
+            yAxisID: index === 0 ? "y-axis-1" : "y-axis-2",
+        })),
+    };
+
+    const options = {
+        responsive: true,
+        scales: {
+            "y-axis-1": {
+                type: "linear",
+                position: "left",
+                display: view === 'portfolioValue',
+                title: {
+                    display: view === 'portfolioValue',
+                    text: 'Portfolio Value ($)',
+                    color: 'brand.600',
+                    font: {
+                        size: 14
+                    }
+                },
+                ticks: {
+                    callback: (value) => `${value}`,
+                },
+
+            },
+            "y-axis-2": {
+                type: "linear",
+                position: "left",
+                display: view === 'performance',
+                title: {
+                    display: view === 'performance',
+                    text: 'Performance (%)',
+                    color: 'brand.600',
+                    font: {
+                        size: 14
+                    }
+                },
+                ticks: {
+                    callback: (value) => `${value * 100}`,
+                },
+            },
+
+            x: {
+                title: {
+                    display: true,
+                    text: `Time`,
+                    color: 'brand.600',
+                    font: {
+                        size: 14
+                    }
+                },
+                ticks: {
+                    callback: function (value, index, ticks) {
+                        // Show only the first and last tick
+                        if (index === 0 || index === ticks.length - 2) {
+                            return this.getLabelForValue(value);
+                        } else {
+                            return ''; // Return an empty string for all other ticks
+                        }
+                    },
+                },
+            },
+
+
+        }
+    };
+
+    return <Line data={data} options={options} />;
+}
+
+export default LineChart2;
+
