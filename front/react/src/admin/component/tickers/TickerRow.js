e1505ca026fab67b74c743741d60d60e16ba1f5a
xlpy <xenalimpinying@gmail.com>
2024-08-12 00:08:28 +0800

    STYLE: ticker page box width

--- front/react/src/admin/component/tickers/TickerRow.js
+++ front/react/src/admin/component/tickers/TickerRow.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Tr, Td, Button, AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay, useDisclosure } from '@chakra-ui/react';
 
 const TickerRow = ({ ticker, deleteTicker }) => {
--- front/react/src/admin/pages/ManageTickersPage.js
+++ front/react/src/admin/pages/ManageTickersPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, useDisclosure, Spinner, Heading, Flex } from '@chakra-ui/react';
+import { Box, Button, useDisclosure, Spinner, Heading, Flex, Container } from '@chakra-ui/react';
 import useTickers from '../hooks/useTickers';
 import TickerTable from '../component/tickers/TickerTable';
 import AddTickerModal from '../component/tickers/AddTickerModal';
@@ -15,30 +15,36 @@ const ManageTickersPage = () => {
     };
 
     return (
-        <Box p={6} maxWidth="1200px" mx="auto">
-            <Flex justify="space-between" align="center" mb={4}>
-                <Heading as="h1" size="lg">Manage Tickers</Heading>
-                <Button colorScheme="teal" onClick={onOpen}>
-                    Add Ticker
-                </Button>
-            </Flex>
-            {loading ? (
-                <Flex justify="center" align="center" height="200px">
-                    <Spinner size="xl" />
-                </Flex>
-            ) : (
-                <TickerTable
-                    activeTickers={activeTickers}
-                    deleteTicker={deleteTicker}
-                />
-            )}
-            <AddTickerModal
-                isOpen={isOpen}
-                onClose={onClose}
-                availableTickers={availableTickers}
-                activeTickers={activeTickers}
-                addTicker={handleAddTicker}  // Pass the updated handleAddTicker function
-            />
+        <Box bg="#666db3" minH="100vh" py={10}>
+            <Container maxW="container.xl" p={4}>
+                <Box bg="white" borderRadius="md" p={8} boxShadow="lg" maxW="90%" mx="auto">
+                    <Flex justify="space-between" align="center" mb={6}>
+                        <Heading as="h1" size="lg" color="#4B4BB3">
+                            Manage Tickers
+                        </Heading>
+                        <Button colorScheme="teal" onClick={onOpen}>
+                            Add Ticker
+                        </Button>
+                    </Flex>
+                    {loading ? (
+                        <Flex justify="center" align="center" height="200px">
+                            <Spinner size="xl" />
+                        </Flex>
+                    ) : (
+                        <TickerTable
+                            activeTickers={activeTickers}
+                            deleteTicker={deleteTicker}
+                        />
+                    )}
+                    <AddTickerModal
+                        isOpen={isOpen}
+                        onClose={onClose}
+                        availableTickers={availableTickers}
+                        activeTickers={activeTickers}
+                        addTicker={handleAddTicker}  // Pass the updated handleAddTicker function
+                    />
+                </Box>
+            </Container>
         </Box>
     );
 };
