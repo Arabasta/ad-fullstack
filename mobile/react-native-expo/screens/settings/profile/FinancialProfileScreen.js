2e7c709b1bcb69b54e9a384a919d3e2c1fb0367e
Arabasta <keiyamx@gmail.com>
2024-08-12 06:55:26 +0800

    mobile: add FinancialProfileScreen

--- mobile/react-native-expo/screens/settings/profile/FinancialProfileScreen.js
+++ mobile/react-native-expo/screens/settings/profile/FinancialProfileScreen.js
@@ -1,64 +1,124 @@
-import React from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet } from 'react-native';
 import Container from "../../../components/common/container/Container";
+import ButtonPrimary from '../../../components/common/button/ButtonPrimary';
+import ButtonCancel from '../../../components/common/button/ButtonCancel';
+import SmallDisplayCard from "../../../components/common/card/SmallDisplayCard";
+import ErrorText from "../../../components/common/text/ErrorText";
+import SuccessText from "../../../components/common/text/SuccessText";
+import useFinancialProfile from "../../../hooks/useFinancialProfile";
+import FinancialProfileService from "../../../services/FinancialProfileService";
+import {employmentStatusOptions, experienceOptions, incomeOptions, investmentObjectiveOptions,
+    netWorthOptions, sourceOfWealthOptions} from "../../../constants/FinancialProfileOptions";
+import FinancialProfileForm from "../../../components/forms/FinancialProfileForm";
+import {getLabelFromValue} from "../../../utils/getLabelFromValue";
+import {validateFinancialProfile} from "../../../utils/validation/validateFinancialProfile";
 import Text from "../../../components/common/text/Text";
 
-const FinancialProfileScreen = ({ navigation }) => {
+const FinancialProfileScreen = () => {
+    const { financialProfile, getFinancialProfile } = useFinancialProfile();
+    const [localProfile, setLocalProfile] = useState(null);
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [isEditing, setIsEditing] = useState(false);
+
+    useEffect(() => {
+        if (financialProfile) {
+            setLocalProfile(financialProfile);
+        }
+    }, [financialProfile]);
+
+    const handleSave = async () => {
+        setSuccess('');
+        setError('');
+
+        const { valid, errorMessage } = validateFinancialProfile(localProfile);
+
+        if (!valid) {
+            setError(errorMessage);
+            return;
+        }
+
+        const updatedProfile = {
+            ...localProfile,
+            annualIncome: parseFloat(localProfile.annualIncome),
+            netWorth: parseFloat(localProfile.netWorth),
+        };
+
+        try {
+            const response = await FinancialProfileService.updateFinancialProfile(updatedProfile);
+            setSuccess('Financial profile updated successfully.');
+            setIsEditing(false);
+            await getFinancialProfile();
+        } catch (error) {
+            console.error("Error saving financial profile:", error);
+            setError("Failed to save financial profile. Please try again.");
+            setSuccess('');
+        }
+    };
+
+    const toggleEdit = () => {
+        setIsEditing(!isEditing);
+    };
+
     return (
         <Container>
-            <Text style={styles.heading}>Financial Profile</Text>
+            {!isEditing ? (
+                localProfile ? (
+                    <>
+                        <SmallDisplayCard
+                            label="Employment Status"
+                            value={getLabelFromValue(employmentStatusOptions, localProfile.employmentStatus)}
+                        />
+                        <SmallDisplayCard
+                            label="Annual Income"
+                            value={getLabelFromValue(incomeOptions, localProfile.annualIncome)}
+                        />
+                        <SmallDisplayCard
+                            label="Net Worth"
+                            value={getLabelFromValue(netWorthOptions, localProfile.netWorth)}
+                        />
+                        <SmallDisplayCard
+                            label="Source of Wealth"
+                            value={getLabelFromValue(sourceOfWealthOptions, localProfile.sourceOfWealth)}
+                        />
+                        <SmallDisplayCard
+                            label="Investment Objective"
+                            value={getLabelFromValue(investmentObjectiveOptions, localProfile.investmentObjective)}
+                        />
+                        <SmallDisplayCard
+                            label="Investment Experience"
+                            value={getLabelFromValue(experienceOptions, localProfile.investmentExperience)}
+                        />
 
-            <View style={styles.fieldContainer}>
-                <Text style={styles.label}>Annual Income</Text>
-                <Text style={styles.value}>$60,000 - $80,000</Text>
-            </View>
+                        {success && (<SuccessText>{success}</SuccessText>)}
 
-            <View style={styles.fieldContainer}>
-                <Text style={styles.label}>Net Worth</Text>
-                <Text style={styles.value}>$100,000 - $200,000</Text>
-            </View>
+                        <ButtonPrimary style={styles.buttonSpacing} title="Edit" onPress={toggleEdit} />
+                    </>
+                ) : ( <Text>Loading...</Text> )
+            ) : (
+                <>
+                    <FinancialProfileForm profile={localProfile} onChange={setLocalProfile} />
 
-            <View style={styles.fieldContainer}>
-                <Text style={styles.label}>Investment Experience</Text>
-                <Text style={styles.value}>Intermediate</Text>
-            </View>
+                    {error && (<ErrorText>{error}</ErrorText>)}
 
-            <TouchableOpacity style={styles.button} onPress={() => console.log('Edit Pressed')}>
-                <Text style={styles.buttonText}>Edit Financial Profile</Text>
-            </TouchableOpacity>
+                    <View style={styles.buttonContainer}>
+                        <ButtonCancel style={styles.buttonSpacing} title="Cancel" onPress={toggleEdit} />
+                        <ButtonPrimary style={styles.buttonSpacing} title="Save" onPress={handleSave} />
+                    </View>
+                </>
+            )}
         </Container>
     );
 };
 
 const styles = StyleSheet.create({
-    heading: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
+    buttonSpacing: {
+        marginTop: 20,
     },
-    fieldContainer: {
-        paddingVertical: 15,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-    },
-    label: {
-        fontSize: 16,
-        fontWeight: 'bold',
-    },
-    value: {
-        fontSize: 16,
-        marginTop: 5,
-    },
-    button: {
-        marginTop: 30,
-        paddingVertical: 15,
-        backgroundColor: '#000',
-        borderRadius: 5,
-        alignItems: 'center',
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 16,
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
 });
 
