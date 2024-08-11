8bafe34605f687c28bb401d5aa5ddaeb4623ad25
めっちゃくちゃ <167387642+123rand456@users.noreply.github.com>
2024-08-11 17:30:46 +0800

    front-end-react(STYLE): LiveTrading Page

--- front/react/src/admin/component/LiveTrading.js
+++ front/react/src/admin/component/LiveTrading.js
@@ -1,65 +1,130 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import useLiveTrading from "../hooks/useLiveTrading";
+import CallToActionSection from "./sections/CallToActionSection";
+import Button from "../../components/common/buttons/Button";
+import {FormControl, FormLabel, GridItem, HStack, Select, useToast, VStack} from "@chakra-ui/react";
+import TransactionsPage from "../pages/LiveTrading/TransactionsPage";
+import {Modal} from "../../components/common/modal/Modal";
 
 const LiveTrading = () => {
-    const navigate = useNavigate();
-    const { message, isTrading, startLiveTrading, stopLiveTrading, getLiveTradingTransactions } = useLiveTrading();
+    const toast = useToast();
+    const {isTrading, startLiveTrading, stopLiveTrading, getLiveTradingTransactions } = useLiveTrading();
     const [portfolioType, setPortfolioType] = useState('AGGRESSIVE');
     const [tickerType, setTickerType] = useState('CRYPTO');
 
     const handleStartLiveTrading = async () => {
         await startLiveTrading();
+        toast({
+            title: "Trading started.",
+            description: "Live trading has been started successfully.",
+            status: "success",
+            duration: 2000,
+            isClosable: true,
+            position:"top"
+        });
     };
 
     const handleStopLiveTrading = async () => {
         await stopLiveTrading();
+        toast({
+            title: "Trading stopped.",
+            description: "Live trading has been stopped.",
+            status: "info",
+            duration: 5000,
+            isClosable: true,
+            position:"top"
+        });
     };
 
     const handleGetTransactions = async () => {
         await getLiveTradingTransactions(portfolioType);
-        navigate(`/admin/transactions?portfolioType=${portfolioType}`);
+        toast({
+            title: "Transactions retrieved.",
+            description: "Live trading transactions have been retrieved.",
+            status: "success",
+            duration: 3000,
+            isClosable: true,
+            position:"top"
+        });
+        //setShowTransactions(true);  // Show the TransactionsPage
+        //navigate(`/admin/transactions?portfolioType=${portfolioType}`);
     };
+    const portfolioOptions = [
+        { label: 'Aggressive', value: 'AGGRESSIVE' },
+        { label: 'Conservative', value: 'CONSERVATIVE' },
+        { label: 'Moderate', value: 'MODERATE' }
+    ];
+
+    const tickerOptions = [
+        { label: 'Crypto', value: 'CRYPTO' },
+        { label: 'Stocks', value: 'STOCKS' }
+    ];
 
     return (
         <div>
-            <div>
-                <label>
-                    Portfolio Type:
-                    <select value={portfolioType} onChange={(e) => setPortfolioType(e.target.value)} disabled={isTrading}>
-                        <option value="AGGRESSIVE">Aggressive</option>
-                        <option value="CONSERVATIVE">Conservative</option>
-                        <option value="MODERATE">Moderate</option>
-                    </select>
-                </label>
-                <label>
-                    Ticker Type:
-                    <select value={tickerType} onChange={(e) => setTickerType(e.target.value)} disabled={isTrading}>
-                        <option value="CRYPTO">Crypto</option>
-                        <option value="STOCKS">Stocks</option>
-                    </select>
-                </label>
-            </div>
-            <div style={{ marginTop: '20px' }}>
-                {isTrading ? (
-                    <button onClick={handleStopLiveTrading} style={{ width: '150px' }}>
-                        Stop Live Trading
-                    </button>
-                ) : (
-                    <button onClick={handleStartLiveTrading} style={{ width: '150px' }}>
-                        Start Live Trading
-                    </button>
-                )}
-            </div>
-            <button onClick={handleGetTransactions} style={{ marginTop: '10px' }}>
-                Get Transactions
-            </button>
+            <CallToActionSection>
+                <VStack>
+                    <HStack>
+                        <FormControl as={GridItem} colSpan={[6, 3]}>
+                            <FormLabel>Portfolio Type</FormLabel>
+                            <Select
+                                value={portfolioType}
+                                onChange={(e) => setPortfolioType(e.target.value)}
+                                disabled={isTrading}
+                                bg="gray.50"
+                                borderColor="gray.300"
+                                _hover={{ borderColor: "gray.400" }}
+                                _focus={{ borderColor: "blue.500", boxShadow: "0 0 0 1px blue.500" }}
+                                _disabled={{ bg: "gray.100", cursor: "not-allowed" }}
+                                width="200px"
+                            >
+                                {portfolioOptions.map(option => (
+                                    <option key={option.value} value={option.value}>
+                                        {option.label}
+                                    </option>
+                                ))}
+                            </Select>
+                        </FormControl>
 
-            {message && (
-                <p style={{ color: isTrading ? 'green' : 'red', marginTop: '20px' }}>
-                    {message}
-                </p>
-            )}
+                        <FormControl as={GridItem} colSpan={[6, 3]}>
+                            <FormLabel>Ticker Type</FormLabel>
+                            <Select
+                                value={tickerType}
+                                onChange={(e) => setTickerType(e.target.value)}
+                                disabled={isTrading}
+                                bg="gray.50"
+                                borderColor="gray.300"
+                                _hover={{ borderColor: "gray.400" }}
+                                _focus={{ borderColor: "blue.500", boxShadow: "0 0 0 1px blue.500" }}
+                                _disabled={{ bg: "gray.100", cursor: "not-allowed" }}
+                                width="100px"
+                            >
+                                {tickerOptions.map(option => (
+                                    <option key={option.value} value={option.value}>
+                                        {option.label}
+                                    </option>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </HStack>
+
+                    <HStack>
+                        <Button onClick={isTrading ? handleStopLiveTrading : handleStartLiveTrading}
+                                style={{width: '150px'}}>
+                            {isTrading ? 'Stop Live Trading' : 'Start Live Trading'}
+                        </Button>
+                        <Modal
+                            triggerText="Get Transactions"
+                            title={`Transactions for ${portfolioType}`}
+                            onOpen={handleGetTransactions}
+                            onClose={() => {}}
+                        >
+                            <TransactionsPage portfolioType={portfolioType}/>
+                        </Modal>
+
+                    </HStack>
+                </VStack>
+            </CallToActionSection>
         </div>
     );
 };
