cc74f40096e7bc01ea3847a59ffd73bcf5d42567
xlpy <xenalimpinying@gmail.com>
2024-08-12 03:39:31 +0800

    STYLE: NewsPage.js

--- front/react/src/pages/NewsPage.js
+++ front/react/src/pages/NewsPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {Flex, Spinner, Text, Container, Divider } from '@chakra-ui/react';
+import { Flex, Spinner, Text, Container, Divider, Box } from '@chakra-ui/react';
 import Heading from "../components/common/text/Heading";
 import NewsList from "../components/news/NewsList";
 import newsService from "../services/NewsService";
@@ -30,24 +30,44 @@ const NewsPage = () => {
     }, []);
 
     if (loading) return (
-        <Flex justify="center" align="center" h="100vh">
-            <Spinner size="xl" color="blue.500" />
+        <Flex justify="center" align="center" h="100vh" bg="brand.400" _dark={{ bg: "#111" }}>
+            <Spinner size="xl" color="brand.500" />
         </Flex>
     );
     if (error) return (
-        <Flex justify="center" align="center" h="100vh">
+        <Flex justify="center" align="center" h="100vh" bg="brand.400" _dark={{ bg: "#111" }}>
             <Text color="red.500" fontSize="xl">{error}</Text>
         </Flex>
     );
 
     return (
-        <Container maxW="7xl" p={4}>
-            <Heading variant="h1" textAlign="center" mb={4} color="blue.700">
-                Breaking News and Insights
-            </Heading>
-            <Divider borderColor="blue.300" mb={8} />
-            <NewsList news={news} />
-        </Container>
+        <Flex
+            bg="brand.400"
+            _dark={{ bg: "#111" }}
+            p={10}
+            justifyContent="center"
+            alignItems="center"
+            w="full"
+        >
+            <Box
+                shadow="xl"
+                bg="white"
+                _dark={{ bg: "gray.800" }}
+                px={{ base: 6, md: 8 }}
+                py={{ base: 10, md: 20 }}
+                mx="auto"
+                borderRadius="md"
+                w="100%"
+            >
+                <Container maxW="7xl" p={4}>
+                    <Heading variant="h1" textAlign="center" mb={4} color="brand.500">
+                        Breaking News and Insights
+                    </Heading>
+                    <Divider borderColor="brand.300" mb={8} />
+                    <NewsList news={news} />
+                </Container>
+            </Box>
+        </Flex>
     );
 };
 
