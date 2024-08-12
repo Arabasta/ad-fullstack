b00e9e881019be8743e11d14d8e9f8ee14227bec
luca <xiongkun778@163.com>
2024-08-12 19:28:50 +0800

    frontend: add news route to admin

--- front/react/src/admin/Routes/adminRoutes.js
+++ front/react/src/admin/Routes/adminRoutes.js
@@ -7,12 +7,14 @@ import BackTestResultPage from "../pages/Backtest/BackTestResultPage";
 import TransactionsPage from "../pages/LiveTrading/TransactionsPage";
 import React from "react";
 import HomePage from "../../pages/HomePage";
+import NewsPage from "../../pages/NewsPage";
 
 const adminRoutes = (
     <>
             <Route key="account" path="/admin/manage-user" element={<AdminViewUsersPage />} />
             <Route key="manage-tickers" path="/admin/manage-tickers" element={<ManageTickersPage />} />
-             <Route key="home" path="/" element={<HomePage />} />,
+            <Route key="home" path="/" element={<HomePage />} />
+            <Route key="news" path="/news" element={<NewsPage />} />,
             <Route key="backTest" path="/admin/backtest" element={<BackTestPage />} />
             <Route key="manageLiveTrading" path="/admin/managelivetrading" element={<AdminManageLiveTradingPage />} />
             <Route key="backTestResult" path="/admin/backtest-result" element={<BackTestResultPage />} />
