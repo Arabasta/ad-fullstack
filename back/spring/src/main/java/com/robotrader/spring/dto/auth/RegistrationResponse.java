fb973e6e44d41bf4e78c4e604e5d631eaf87b3f8
Arabasta <keiyamx@gmail.com>
2024-08-12 11:06:52 +0800

    back spring: add Role to RegistrationResponse and AuthenticationResponse

--- back/spring/src/main/java/com/robotrader/spring/dto/auth/RegistrationResponse.java
+++ back/spring/src/main/java/com/robotrader/spring/dto/auth/RegistrationResponse.java
@@ -1,5 +1,6 @@
 package com.robotrader.spring.dto.auth;
 
+import com.robotrader.spring.model.enums.RoleEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,11 +14,13 @@ public class RegistrationResponse {
     private String message;
     private String username;
     private String jwtToken;
+    private RoleEnum role;
     private LocalDateTime timestamp;
 
-    public RegistrationResponse(String jwtToken, String username) {
+    public RegistrationResponse(String jwtToken, String username, RoleEnum role) {
         this.jwtToken = jwtToken;
         this.username = username;
+        this.role = role;
         this.timestamp = LocalDateTime.now();
     }
 }
