30e7612831d7808fb3139addb54a03125c3e5438
xlpy <xenalimpinying@gmail.com>
2024-08-15 01:30:01 +0800

    add validation to UpdateBankDetailsDTO.java

--- back/spring/src/main/java/com/robotrader/spring/dto/bankDetails/UpdateBankDetailsDTO.java
+++ back/spring/src/main/java/com/robotrader/spring/dto/bankDetails/UpdateBankDetailsDTO.java
@@ -1,5 +1,8 @@
 package com.robotrader.spring.dto.bankDetails;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +13,17 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateBankDetailsDTO {
+
+    @NotBlank(message = "Bank name cannot be blank")
+    @Size(max = 20, message = "Bank name must not exceed 20 characters")
     private String bankName;
+
+    @NotBlank(message = "Account number cannot be blank")
+    @Size(min = 8, max = 20, message = "Account number must be between 8 and 20 characters")
+    @Pattern(regexp = "\\d+", message = "Account number must contain only digits")
     private String accountNumber;
+
+    @NotBlank(message = "Account holder name cannot be blank")
+    @Size(max = 50, message = "Account holder name must not exceed 50 characters")
     private String accountHolderName;
 }
