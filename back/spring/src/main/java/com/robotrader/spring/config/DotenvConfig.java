1452afdefcb79524911b4182661abe77cfc964c0
Arabasta <keiyamx@gmail.com>
2024-08-15 07:04:23 +0800

    deploy spring remove DotenvConfig.java

--- back/spring/src/main/java/com/robotrader/spring/config/DotenvConfig.java
+++ back/spring/src/main/java/com/robotrader/spring/config/DotenvConfig.java
@@ -1,16 +1,16 @@
-package com.robotrader.spring.config;
-
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-// https://github.com/cdimascio/dotenv-java
-@Configuration
-public class DotenvConfig {
-
-    @Bean
-    public Dotenv dotenv() {
-        return Dotenv.load();
-    }
-}
-
+//package com.robotrader.spring.config;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//// https://github.com/cdimascio/dotenv-java
+//@Configuration
+//public class DotenvConfig {
+//
+//    @Bean
+//    public Dotenv dotenv() {
+//        return Dotenv.load();
+//    }
+//}
+//
