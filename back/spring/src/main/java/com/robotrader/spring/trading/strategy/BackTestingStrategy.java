48f637acbbe19282ed7b63f0494ddb3dad167dc3
Arabasta <keiyamx@gmail.com>
2024-08-15 05:58:53 +0800

    deploy spring

--- back/spring/src/main/java/com/robotrader/spring/trading/strategy/BackTestingStrategy.java
+++ back/spring/src/main/java/com/robotrader/spring/trading/strategy/BackTestingStrategy.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.robotrader.spring.dto.ticker.TickerDTO;
 import com.robotrader.spring.model.enums.PortfolioTypeEnum;
 import com.robotrader.spring.model.enums.TickerTypeEnum;
+import com.robotrader.spring.trading.algorithm.base.TradingAlgorithmBase;
 import com.robotrader.spring.trading.dto.PredictionDTO;
 import com.robotrader.spring.trading.dto.TradeTransaction;
-import com.robotrader.spring.trading.algorithm.base.TradingAlgorithmBase;
 import com.robotrader.spring.trading.interfaces.TradePersistence;
 import com.robotrader.spring.trading.interfaces.TradingStrategy;
 import com.robotrader.spring.trading.service.HistoricalMarketDataService;
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -124,39 +125,39 @@ public class BackTestingStrategy implements TradingStrategy {
     }
 
     public Mono<PredictionDTO> getPricePredictions(Map<String, List<Object>> marketDataHistory, String tickerName) {
-        List<BigDecimal> historicalPrices = marketDataHistory.get("close").stream()
-                .map(price -> (BigDecimal) price)
-                .collect(Collectors.toList());
-
-        PredictionDTO predictionDTO = new PredictionDTO();
-        predictionDTO.setPredictions(historicalPrices);
-
-        TickerDTO tickerDTO = new TickerDTO();
-        tickerDTO.setTickerName(tickerName);
-        tickerDTO.setTickerType(TickerTypeEnum.STOCKS);
-        tickerDTO.setPortfolioType(PortfolioTypeEnum.AGGRESSIVE);
-        predictionDTO.setTickerDTO(tickerDTO);
-
-        return Mono.just(predictionDTO);
-        // todo: replace above with below code for deployment. Above code doesn't require fast api prediction server to be set up
-//        PredictionDTO predictionDTO = new PredictionDTO();
-//        List<BigDecimal> historicalVW = marketDataHistory.get("vw").subList(0, MIN_INPUT_SIZE + 1)
-//                .stream()
-//                .map(obj -> new BigDecimal(obj.toString()))
+//        List<BigDecimal> historicalPrices = marketDataHistory.get("close").stream()
+//                .map(price -> (BigDecimal) price)
 //                .collect(Collectors.toList());
-//        predictionDTO.setPredictions(historicalVW);
+//
+//        PredictionDTO predictionDTO = new PredictionDTO();
+//        predictionDTO.setPredictions(historicalPrices);
+//
 //        TickerDTO tickerDTO = new TickerDTO();
-//        tickerDTO.setTickerName(processTicker(tickerName));
+//        tickerDTO.setTickerName(tickerName);
 //        tickerDTO.setTickerType(TickerTypeEnum.STOCKS);
 //        tickerDTO.setPortfolioType(PortfolioTypeEnum.AGGRESSIVE);
 //        predictionDTO.setTickerDTO(tickerDTO);
-//        Mono<PredictionDTO> predictionDTOMono;
-//        try {
-//            predictionDTOMono = predictionService.byPredictionDtoBacktest(predictionDTO);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return predictionDTOMono;
+//
+//        return Mono.just(predictionDTO);
+        // todo: replace above with below code for deployment. Above code doesn't require fast api prediction server to be set up
+        PredictionDTO predictionDTO = new PredictionDTO();
+        List<BigDecimal> historicalVW = marketDataHistory.get("vw").subList(0, MIN_INPUT_SIZE + 1)
+                .stream()
+                .map(obj -> new BigDecimal(obj.toString()))
+                .collect(Collectors.toList());
+        predictionDTO.setPredictions(historicalVW);
+        TickerDTO tickerDTO = new TickerDTO();
+        tickerDTO.setTickerName(processTicker(tickerName));
+        tickerDTO.setTickerType(TickerTypeEnum.STOCKS);
+        tickerDTO.setPortfolioType(PortfolioTypeEnum.AGGRESSIVE);
+        predictionDTO.setTickerDTO(tickerDTO);
+        Mono<PredictionDTO> predictionDTOMono;
+        try {
+            predictionDTOMono = predictionService.byPredictionDtoBacktest(predictionDTO);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return predictionDTOMono;
     }
 
     @Override
