b47474e4799c4d4597a667c11e4e06787b398d6e
Arabasta <keiyamx@gmail.com>
2024-08-16 04:51:11 +0800

    front react: style 404 page

--- front/react/src/pages/error/NotFoundPage.js
+++ front/react/src/pages/error/NotFoundPage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Box, Flex } from '@chakra-ui/react';
+import { WarningIcon } from '@chakra-ui/icons';
+import Text from "../../components/common/text/Text";
 
 const NotFoundPage = () => {
     return (
-        <div>
-            <h2>404 - Page Not Found</h2>
-        </div>
+        <Flex direction="column" align="center" justify="center" height="100vh" bg="gray.800">
+            <Box mb={4}>
+                <WarningIcon boxSize={12} color="red.500" />
+            </Box>
+            <Text variant="h1" fontSize="4xl" color="red.500" mb={4}>
+                404 - Page Not Found
+            </Text>
+            <Text variant="p" fontSize="lg" color="white">
+                The page you are looking for doesn't exist or has been moved.
+            </Text>
+        </Flex>
     );
 };
 
