4d2616bd4fde0bf893d86e0f304ba4732789a6de
Arabasta <keiyamx@gmail.com>
2024-08-18 14:50:36 +0800

    back spring: remove IPaymentService

--- back/spring/src/main/java/com/robotrader/spring/service/interfaces/IPaymentService.java
+++
@@ -1,8 +0,0 @@
-package com.robotrader.spring.service.interfaces;
-
-import com.robotrader.spring.dto.paymentWithdrawal.PaymentRequestDTO;
-import com.robotrader.spring.dto.paymentWithdrawal.PaymentResponseDTO;
-
-public interface IPaymentService {
-    PaymentResponseDTO processPayment(PaymentRequestDTO paymentRequestDTO);
-}
